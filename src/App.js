53220cd25fffb88ac1a036908ad4248f3ab69355
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-26 19:55:14 -0500

    updated files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { Route, Router, Routes } from "react-router-dom";
-
+import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" />
-      </Routes>
-    </Router>
+    <Home />
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,35 @@
 import { AppBar, Container, Toolbar, Typography } from "@mui/material"
 import AdbIcon from '@mui/icons-material/Adb';
 
+const pages = ['Products', 'Pricing', 'Blog'];
+
 const Navbar = () => {
     return (
-        <AppBar position="static">
+        <AppBar>
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
                     <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="a"
-                            href="/"
-                            sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                            }}
-                        >
-                            LOGO
-                        </Typography>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                        mr: 2,
+                        display: { xs: 'none', md: 'flex' },
+                        fontFamily: 'monospace',
+                        fontWeight: 700,
+                        letterSpacing: '.3rem',
+                        color: 'inherit',
+                        textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
                 </Toolbar>
             </Container>
         </AppBar>
     )
 }
+
+export default Navbar;
