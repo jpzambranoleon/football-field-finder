e224ca21d2a22e0d8d48b42f3a00579c12de53e4
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-26 23:40:10 -0500

    updated files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,18 @@
 import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material"
 import AdbIcon from '@mui/icons-material/Adb';
-import { Menu } from "@mui/icons-material";
+import { Camera, Menu } from "@mui/icons-material";
 
 const pages = ['Home', 'About'];
 
 const Navbar = () => {
 
     return (
-        <AppBar>
+        <AppBar position="relative">
             <Toolbar>
-                <Box sx={{ flexGrow: 1, display: 'flex' }}>
-                    <AdbIcon sx={{ mr: 1 }} />
-                    <Typography>
-                        Logo
-                    </Typography>
-                </Box>
-
-
-                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                    {pages.map((page) => (
-                    <Button
-                        key={page}
-                        sx={{ my: 2, color: 'white', display: 'block' }}
-                    >
-                        {page}
-                    </Button>
-                    ))}
-                </Box>
-
-                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                    <Button color="inherit">Login</Button>
-                </Box>
-                <Box sx={{ display: { xs: 'flex', md: 'none'} }}>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"    
-                    >
-                        <Menu />
-                    </IconButton>
-                </Box>
+                <Camera sx={{ mr: 2 }} />
+                <Typography variant="h6" color="inherit" noWrap>
+                    Album layout
+                </Typography>
             </Toolbar>
         </AppBar>
     )
