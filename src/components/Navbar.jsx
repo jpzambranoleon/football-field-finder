f5d009f2dc2ee766d588befd8a3e9efbf68b506d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-26 21:38:09 -0500

    updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import { AppBar, Avatar, Box, Button, Container, IconButton, Menu, Toolbar, Tooltip, Typography } from "@mui/material"
+import { AppBar, Box, Button, Container, IconButton, Toolbar, Typography } from "@mui/material"
 import AdbIcon from '@mui/icons-material/Adb';
-import { useState } from "react";
+import { Menu } from "@mui/icons-material";
 
 const pages = ['Home', 'About'];
 
@@ -8,45 +8,37 @@ const Navbar = () => {
 
     return (
         <AppBar>
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
+            <Toolbar>
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    {pages.map((page) => (
+                    <Button
+                        key={page}
+                        sx={{ my: 2, color: 'white', display: 'block' }}
                     >
-                        LOGO
-                    </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                        <Button
-                            key={page}
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                        >
-                            {page}
-                        </Button>
-                        ))}
-                    </Box>
+                        {page}
+                    </Button>
+                    ))}
+                </Box>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Button sx={{ my: 2, color: 'white', display: 'block' }}>
-                            English
-                        </Button>
-                    </Box>
 
-                </Toolbar>
-            </Container>
+
+
+
+
+                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                    <Button color="inherit">Login</Button>
+                </Box>
+                <Box sx={{ display: { xs: 'flex', md: 'none'} }}>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"    
+                    >
+                        <Menu />
+                    </IconButton>
+                </Box>
+            </Toolbar>
         </AppBar>
     )
 }
