e1536538d342856c298c903b0027f083621248ae
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-26 20:34:49 -0500

    updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material"
+import { AppBar, Avatar, Box, Button, Container, IconButton, Menu, Toolbar, Tooltip, Typography } from "@mui/material"
 import AdbIcon from '@mui/icons-material/Adb';
+import { useState } from "react";
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ['Home', 'About'];
 
 const Navbar = () => {
+
     return (
         <AppBar>
             <Container maxWidth="xl">
@@ -15,17 +17,34 @@ const Navbar = () => {
                         component="a"
                         href="/"
                         sx={{
-                        mr: 2,
-                        display: { xs: 'none', md: 'flex' },
-                        fontFamily: 'monospace',
-                        fontWeight: 700,
-                        letterSpacing: '.3rem',
-                        color: 'inherit',
-                        textDecoration: 'none',
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
                         }}
                     >
                         LOGO
                     </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                        <Button
+                            key={page}
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >
+                            {page}
+                        </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Button sx={{ my: 2, color: 'white', display: 'block' }}>
+                            English
+                        </Button>
+                    </Box>
+
                 </Toolbar>
             </Container>
         </AppBar>
