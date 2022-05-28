5531f1d00ba998582ae9001154863b3e4b7f3034
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-28 00:21:02 -0500

    updated Post.jsx

--- src/pages/home/components/Post.jsx
+++ src/pages/home/components/Post.jsx
@@ -1,13 +1,25 @@
 import { Groups } from "@mui/icons-material";
-import { Avatar, Card } from "@mui/material";
+import { Avatar, Box, Button, Card, CardActionArea, CardContent, Typography } from "@mui/material";
 
 const Post = () => {
 
     return (
-        <Card fullwidth>
-            <Avatar>
-                <Groups />
-            </Avatar>
+        <Card>
+            <CardActionArea>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Avatar sx={{ ml: 2, mt: 2, mb: 2, width: '50px', height: '50px' }} >
+                        <Groups fontSize="large"/>
+                    </Avatar>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            Word of the Day
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                            Players Needed
+                        </Typography>
+                    </CardContent>
+                </Box>
+            </CardActionArea>
         </Card>
     )
 }
