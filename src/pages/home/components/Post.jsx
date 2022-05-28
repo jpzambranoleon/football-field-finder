adafb93d7727f0d64b4b93c127093e9022846a49
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-27 23:36:10 -0500

    updated Post.jsx

---
+++ src/pages/home/components/Post.jsx
@@ -0,0 +1,15 @@
+import { Groups } from "@mui/icons-material";
+import { Avatar, Card } from "@mui/material";
+
+const Post = () => {
+
+    return (
+        <Card fullwidth>
+            <Avatar>
+                <Groups />
+            </Avatar>
+        </Card>
+    )
+}
+
+export default Post;
