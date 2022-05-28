346c661b5fff12532363881db63a3cb7a0c14fbf
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-28 18:07:37 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Route, Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/home/Home";
+import PostPage from "./pages/post/PostPage";
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<PostPage />} />
+      </Routes>
+    </Router>
   );
 }
 
