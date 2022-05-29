cdaaee84b2ccace6cd04ceb6544385fd9606aa4d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-29 16:21:16 -0500

    updated About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -34,10 +34,17 @@ export default function About() {
         </Box>
         
         <Container maxWidth="100px">
-        <Grid container>
+        <Grid container spacing={3}>
             <Grid item xs={12} sm={8}>
                 <Typography>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis eius dicta autem amet molestias eligendi voluptatum aliquam iure, deleniti in, voluptatibus modi ut repudiandae, repellat pariatur quisquam omnis libero numquam!
+                    This website was created as a project for bringing people closer together. 
+                    As an advid football fan, I've often sought out groups of people that enjoy playing the sport. This website is designed to help people who want to find group/groups of advid football fans that love to play the sport.
+                </Typography>
+                <Typography sx={{ mt: 2 }}>
+                    The sport of football/soccer is becoming increasingly popular in the United States, so we decided to contribute to the growth in our own way. We designed a website that allows users to post 
+                </Typography>
+                <Typography sx={{ mt: 2 }}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis repellendus perferendis iusto magni eius. Ea, ipsum? Saepe accusamus odio rem maiores, ipsa est nostrum ab iure molestias, hic praesentium tempora? 
                 </Typography>
             </Grid>
             <Grid item xs={12} sm={4}>
