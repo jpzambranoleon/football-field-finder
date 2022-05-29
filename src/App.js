acae86e2da35c189ff833d6fbfcf54fb14556b1e
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-29 18:52:12 -0500

    updated Login.jsx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/about/About";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import PostPage from "./pages/post/PostPage";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/post" element={<PostPage />} />
           <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,55 +1,86 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, createTheme, CssBaseline, Grid, Link, Stack, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import { SportsSoccer } from "@mui/icons-material";
+import { Avatar, Box, Button, Card, CardActions, CardContent, CardMedia, Checkbox, Container, createTheme, CssBaseline, FormControlLabel, Grid, Link, Paper, Stack, TextField, ThemeProvider, Toolbar, Typography } from "@mui/material";
 import Navbar from "../../components/Navbar";
-import FeedFilter from "./components/FeedFilter";
-import Post from "./components/Post";
 
-function Copyright() {
+function Copyright(props) {
     return (
-      <Typography variant="body2" color="text.secondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
-          Leone Digitale
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Leone Digitale
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
     );
   }
 
 export default function Login() {
-  return (
-    <>
-        <CssBaseline />
-        <Box sx={{ bgcolor: 'background.paper', pt: 6, pb: 6 }}>
-            <Container maxWidth="sm">
-                <Typography
-                    component="h1"
-                    variant="h4"
-                    align="center"
-                    color="text.primary"
-                    gutterBottom
-                >
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{ 
+                    mt: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                 }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <SportsSoccer />
+                </Avatar>
+                <Typography component="h1" variant="h5">
                     Sign in
                 </Typography>
-            </Container>
-        </Box>
-
-        {/* Footer */}
-        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-            Footer
-        </Typography>
-        <Typography
-            variant="subtitle1"
-            align="center"
-            color="text.secondary"
-            component="p"
-        >
-            Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-        </Box>
-        {/* End footer */}
-    </>
-  );
+                <Box component="form">
+                    <TextField 
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel 
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+    );
 }
