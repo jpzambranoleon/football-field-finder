a3000efa8e7c3ce088c90c2c576a11baf8a1577c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-29 23:44:16 -0500

    updated Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -23,36 +23,31 @@ export default function Home() {
       <Navbar />
       <main>
         {/* Hero unit */}
-        <Box
-            sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
-            }}
+        <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}
         >
-            <Container maxWidth="sm">
-                <Typography
-                    component="h1"
-                    variant="h2"
-                    align="center"
-                    color="text.primary"
-                    gutterBottom
-                >
-                    Team Finder
-                </Typography>
-                <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                    Looking for some people to play football with? Are you looking for a team to join? We can help you with that. Find a team near you!
-                </Typography>
-                <Stack
-                    sx={{ pt: 4 }}
-                    direction="row"
-                    spacing={2}
-                    justifyContent="center"
-                >
-                    <Button variant="contained">Main call to action</Button>
-                    <Button variant="outlined">Secondary action</Button>
-                </Stack>
-            </Container>
+          <Container maxWidth="sm">
+            <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+            >
+                Team Finder
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                Looking for some people to play football with? Are you looking for a team to join? We can help you with that. Find a team near you!
+            </Typography>
+            <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={2}
+                justifyContent="center"
+            >
+                <Button variant="contained">Main call to action</Button>
+                <Button variant="outlined">Secondary action</Button>
+            </Stack>
+          </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
             <FeedFilter />
