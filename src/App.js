ec41cf2ed761e893ddd798534d94ccb6ef1fb51b
Jean-Paul <jpzambrano.leon@gmail.com>
2022-05-29 21:05:44 -0500

    updated files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/about/About";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+import Login from "./authorization/Login";
 import PostPage from "./pages/post/PostPage";
 
 function App() {
