920d7b0b3d4c655f4341c4b4b62a25e99cd36f95
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-07 18:30:25 -0500

    updated About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,31 +1,26 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, createTheme, CssBaseline, Grid, Link, Stack, ThemeProvider, Toolbar, Typography } from "@mui/material";
-import Navbar from "../../components/Navbar";
-import AboutDescription from "./components/AboutDescription";
-import AboutImage from "./components/AboutImage";
+import { Box, Container, Grid, Typography } from "@mui/material";
 
 export default function About() {
-  return (
-    <>
-        <Box sx={{ bgcolor: 'background.paper', pt: 6, pb: 6 }}>
-            <Container maxWidth="sm">
-                <Typography
-                    component="h1"
-                    variant="h4"
-                    align="center"
-                    color="text.primary"
-                    gutterBottom
-                >
-                    About Project
-                </Typography>
-            </Container>
-        </Box>
-        
-        <Container maxWidth="100px">
-        <Grid container spacing={3}>
-            <AboutDescription />
-            <AboutImage />
-        </Grid>
-        </Container>
-  </>
-  );
+    return (
+       <main>
+           <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
+               <Container>
+                   <Grid container spacing={3}>
+                       <Grid item sx={12} sm={6}>
+                           <Box>
+                               <Typography
+                                    component="h1"
+                                    variant="h3"
+                                    align="center"
+                                    color="success.main"
+                               >
+                                   About
+                               </Typography>
+                           </Box>
+                       </Grid>
+                   </Grid>
+               </Container>
+           </Box>
+       </main> 
+    )
 }
