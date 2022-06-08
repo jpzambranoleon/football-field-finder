5c3242d7390f57dbc83d028f13c69399ca540dcd
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-08 00:51:51 -0500

    updated UserPost.jsx

---
+++ src/pages/user/components/UserPost.jsx
@@ -0,0 +1,37 @@
+import { Sports } from "@mui/icons-material";
+import { Avatar, Box, Button, Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import { pink } from "@mui/material/colors";
+
+const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
+
+const TrainerPost = () => {
+    return (
+        <Card sx={{ mb: 2 }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Avatar sx={{ ml: 2, mt: 2, mb: 2, width: '50px', height: '50px', bgcolor: pink[400] }} >
+                    <Sports fontSize="large"/>
+                </Avatar>
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        3 hrs ago {bull} Trainer name {bull} Hot Springs, AR
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        Trainer looking to offer some training
+                    </Typography>
+                </CardContent>
+            </Box>
+            <Button color="error">
+                Delete
+            </Button>
+        </Card>
+    )
+}
+
+export default TrainerPost;
