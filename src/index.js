1165affcb59739ec0816101350c833a7dc2ced9c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-07 22:12:55 -0500

    updated files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import { createTheme, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-
-const theme = createTheme();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
