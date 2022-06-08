05542b83fafa8ae869c027c23ecedefead7f5f97
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-07 20:06:02 -0500

    updated About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -144,6 +144,35 @@ export default function About() {
                                 </CardActions>
                             </Card>
                         </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                                <CardMedia 
+                                    component="img"
+                                    height="260"
+                                    image="/images/zurab_cio.jpg"
+                                    alt="portrait"
+                                />
+                                <CardContent sx={{ flexGrow: 1 }}>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        Zurab Sabakhtarishvili
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Chief Information Officer, Co-founder
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <IconButton href="https://www.facebook.com/zurab.sabakhtarishvili">
+                                        <Facebook color="primary" />
+                                    </IconButton>
+                                    <IconButton href="https://www.linkedin.com/in/zurab-sabakhtarishvili-33b2731b4/">
+                                        <LinkedIn color="primary" />
+                                    </IconButton>
+                                    <IconButton href="https://github.com/ZSabakh">
+                                        <GitHub color="primary" />
+                                    </IconButton>
+                                </CardActions>
+                            </Card>
+                        </Grid>
                     </Grid>
                </Container>
            </Box>
