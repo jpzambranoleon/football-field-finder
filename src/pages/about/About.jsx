7650a14c11453f336788756f3126274acca41775
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-07 19:41:28 -0500

    updated About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,19 +1,21 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Groups, Person, Sports } from "@mui/icons-material";
+import { Avatar, Box, Card, CardMedia, Container, Grid, Typography } from "@mui/material";
 
 export default function About() {
     return (
        <main>
            <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
                <Container>
-                   <Grid container spacing={3}>
+                   <Grid container spacing={4}>
                        <Grid item sx={12} sm={6}>
                            <Box>
                                <Typography
                                     component="h1"
-                                    variant="h7"
+                                    variant="h4"
                                     align="left"
+                                    color="text.primary"
                                >
-                                   This web app was designed and developed by a small team of talented developers.
+                                   Designed and developed by a small team of talented developers.
                                </Typography>
                                <Typography
                                     variant="h6"
@@ -23,10 +25,60 @@ export default function About() {
                                 >
                                     This web app was created to help people who are interested in the sport of soccer 
                                     to meet up with local teams in the area that are looking for players to join. 
-                                    The sport has seen a rapid popularity growth in the United States, and we wanted
-                                    to contribute to that growth in our own way.
                                 </Typography>
+                                <Grid container spacing={2} align="center">
+                                    <Grid item sx={12} sm={4}>
+                                        <Typography
+                                            variant="h5"
+                                            gutterBottom
+                                        >
+                                            Team
+                                        </Typography>
+                                        <Avatar sx={{ width: 60, height: 60, mb: 2, bgcolor: "secondary.main" }}>
+                                            <Groups fontSize="large" />
+                                        </Avatar>
+                                        <Typography variant="body1" color="text.secondary">
+                                            You can create a team or join an existing one in your area
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item sx={12} sm={4}>
+                                        <Typography
+                                            variant="h5"
+                                            gutterBottom
+                                        >
+                                            Free Agent
+                                        </Typography>
+                                        <Avatar sx={{ width: 60, height: 60, mb: 2, bgcolor: "secondary.main" }}>
+                                            <Person fontSize="large" />
+                                        </Avatar>
+                                        <Typography variant="body1" color="text.secondary">
+                                            Teams can recruit free agents that are looking for a team to join
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item sx={12} sm={4}>
+                                        <Typography
+                                            variant="h5"
+                                            gutterBottom
+                                        >
+                                            Trainer
+                                        </Typography>
+                                        <Avatar sx={{ width: 60, height: 60, mb: 2, bgcolor: "secondary.main" }}>
+                                            <Sports fontSize="large" />
+                                        </Avatar>
+                                        <Typography variant="body1" color="text.secondary">
+                                            If you are an experienced, you can offer training services
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
                            </Box>
+                       </Grid>
+                       <Grid item xs={12} sm={6}>
+                           <Card>
+                               <CardMedia 
+                                    component="img"
+                                    image="/images/soccer-coming-soon.jpg"
+                               />
+                           </Card>
                        </Grid>
                    </Grid>
                </Container>
