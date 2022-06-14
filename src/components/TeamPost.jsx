e8dd4dc5225710b216dc358d8939a7c3baa2f37d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-13 23:20:14 -0500

    updated files

--- src/components/TeamPost.jsx
+++ src/components/TeamPost.jsx
@@ -1,26 +1,29 @@
 import { Groups } from "@mui/icons-material";
-import { Avatar, Box, Card, CardActionArea, CardContent, CardHeader, Grid, Typography } from "@mui/material";
+import { Avatar, Box, Button, Card, CardActionArea, CardActions, CardContent, CardHeader, Grid, Typography } from "@mui/material";
 import { green } from "@mui/material/colors";
 
 const TeamPost = () => {
     return (
         <Grid item xs={12} sm={6} md={4}>
             <Card sx={{ mb: 2, height: '100%', display: 'flex', flexDirection: 'column' }}>
-                <CardActionArea sx={{ height: '100%' }}>
-                    <CardHeader 
-                        avatar={
-                            <Avatar sx={{ bgcolor: green[400] }} >
-                                <Groups />
-                            </Avatar>   
-                        }
-
-                    />
-                    <CardContent sx={{ flexGrow: 1 }}>
-                        <Typography variant="h5" component="div">
-                            Looking for more players to join our team
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
+                <CardHeader 
+                    avatar={
+                        <Avatar sx={{ bgcolor: green[400] }} >
+                            <Groups />
+                        </Avatar>   
+                    }
+                    title="Team Name FC"
+                    subheader="September 14, 2016"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography variant="h5">
+                        Looking for more players to join our team
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" color="success">Share</Button>
+                    <Button size="small" color="success">Learn More</Button>
+                </CardActions>
             </Card>
         </Grid>
     )
--- src/components/TrainerPost.jsx
+++ src/components/TrainerPost.jsx
@@ -1,26 +1,30 @@
 import { Sports } from "@mui/icons-material";
-import { Avatar, Box, Card, CardActionArea, CardContent, CardHeader, Grid, Typography } from "@mui/material";
+import { Avatar, Box, Button, Card, CardActionArea, CardActions, CardContent, CardHeader, Grid, Typography } from "@mui/material";
 import { pink } from "@mui/material/colors";
 
 const TrainerPost = () => {
     return (
         <Grid item xs={12} sm={6} md={4}>
             <Card sx={{ mb: 2, height: '100%', display: 'flex', flexDirection: 'column' }}>
-                <CardActionArea sx={{ height: '100%' }}>
-                    <CardHeader 
-                        avatar={
-                            <Avatar sx={{ bgcolor: pink[400] }} >
-                                <Sports />
-                            </Avatar>   
-                        }
+                <CardHeader 
+                    avatar={
+                        <Avatar sx={{ bgcolor: pink[400] }} >
+                            <Sports />
+                        </Avatar>   
+                    }
+                    title="Trainer Name"
+                    subheader="September 14, 2016"
 
-                    />
-                    <CardContent sx={{ flexGrow: 1 }}>
-                        <Typography variant="h5" component="div">
-                            Looking for more players to join our team
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography variant="h5" color="text.secondary">
+                        Soccer trainer looking to offer some classes
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" color="success">Share</Button>
+                    <Button size="small" color="success">Learn More</Button>
+                </CardActions>
             </Card>
         </Grid>
     )
