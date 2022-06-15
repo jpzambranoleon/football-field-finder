5ec0615825441e1f1e1303f92ae5a570c54d118c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-15 17:37:02 -0500

    updated PostPage.jsx

--- src/pages/post/PostPage.jsx
+++ src/pages/post/PostPage.jsx
@@ -10,16 +10,19 @@ export default function PostPage() {
                 <Container>
                     <Grid container>
                         <Grid item>
-                            <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
-                                <Avatar sx={{ bgcolor: green[400], mr: 2, height: '50px', width: '50px' }} >
+                            <Box sx={{ display: 'flex', alignItems: 'center', mb: 5 }}>
+                                <Avatar sx={{ bgcolor: green[400], mr: 2, height: '55px', width: '55px' }} >
                                     <Groups fontSize="large" />
                                 </Avatar> 
                                 <Typography variant="h5" fontWeight="bold">
                                     Looking for more players to join 
                                 </Typography>
                             </Box>
-                            <Typography paragraph>
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, exercitationem maxime suscipit corrupti recusandae voluptate sapiente est a. Eos itaque deleniti possimus blanditiis cupiditate alias doloremque rem in quidem? Esse!
+                            <Typography variant="body1" paragraph>
+                                We are Team Name FC! We play every Sunday at Lakeside Field Park. 
+                                We are looking for more players to come and join our team.
+                                We are hoping to get enough players to play on the Sunday League Tornament.
+                                We practice on Saturdays from 3:30 to 6:00 pm.
                             </Typography>
                         </Grid>
                         <SimpleTable />
