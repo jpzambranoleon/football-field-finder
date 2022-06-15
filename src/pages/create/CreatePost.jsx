69c188bc6bb6c9fb3c79cc03d0bd18f1e07988d4
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-15 18:30:05 -0500

    updated files

--- src/pages/create/CreatePost.jsx
+++ src/pages/create/CreatePost.jsx
@@ -1,35 +1,32 @@
-import { Box, Container, Grid, Paper, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import CreateTeam from "./components/CreateTeam";
 
 export default function CreatePost() {
     return (
        <main>
            <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
-                <Container>
-                    <Grid container spacing={2} align='center'>
-                        <Grid item>
-                            <Paper>
-                                <Typography>
-                                    Team
-                                </Typography>
-                            </Paper>
-                        </Grid>
-                        <Grid item>
-                            <Paper>
-                                <Typography>
-                                    Free Agent
-                                </Typography>
-                            </Paper>
-                        </Grid>
-                        <Grid item>
-                            <Paper>
-                                <Typography>
-                                    Trainer
-                                </Typography>
-                            </Paper>
-                        </Grid>
-                    </Grid>
+                <Container maxWidth="sm">
+                    <Typography
+                        component="h1"
+                        variant="h2"
+                        align="center"
+                        color="text.primary"
+                        gutterBottom
+                    >
+                        Create A Post
+                    </Typography>
+                    <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                        Use the buttons below to create a post
+                    </Typography>
                 </Container>
            </Box>
+           <Container>
+                <Grid container spacing={5} align='center'>
+                    <CreateTeam />
+                    <CreateTeam />
+                    <CreateTeam />
+                </Grid>
+           </Container>
        </main> 
     )
 };
--- src/pages/create/components/CreateTeam.jsx
+++ src/pages/create/components/CreateTeam.jsx
@@ -1,14 +1,29 @@
-import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { Groups } from "@mui/icons-material";
+import { Avatar, Card, CardActionArea, CardContent, Grid, Typography } from "@mui/material";
+import { green } from "@mui/material/colors";
 
 const CreateTeam = () => {
     return (
-        <Grid item>
+        <Grid item xs={12} sm={4}>
             <Card>
+                <CardActionArea>
                 <CardContent>
-                    <Typography>
+                    <Avatar sx={{ height: '90px', width: '90px', bgcolor: green[400] }}>
+                        <Groups fontSize="large" sx={{ height: '50px', width: '50px' }} />
+                    </Avatar>
+                </CardContent>
+                <CardContent>
+                    <Typography gutterBottom variant="h4">
                         Team
                     </Typography>
+                   <Typography variant="body1" align="center">
+                        Do you have a team and are lookig for 
+                        players to join? Use the team post to
+                        post information on your team and how others
+                        can find and join.
+                   </Typography>
                 </CardContent>
+                </CardActionArea>
             </Card>
         </Grid>
     ); 
