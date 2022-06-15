2288cd459264a0065aeb27ad2db420e6240ca168
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-15 17:49:30 -0500

    created CreateTeam.jsx

---
+++ src/pages/create/components/CreateTeam.jsx
@@ -0,0 +1,17 @@
+import { Card, CardContent, Grid, Typography } from "@mui/material";
+
+const CreateTeam = () => {
+    return (
+        <Grid item>
+            <Card>
+                <CardContent>
+                    <Typography>
+                        Team
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Grid>
+    ); 
+};
+
+export default CreateTeam;
