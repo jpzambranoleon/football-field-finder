71d9eb079b7d29d0b49379480e422275085f8ca1
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-15 00:49:21 -0500

    updated PostPage.jsx

--- src/pages/post/PostPage.jsx
+++ src/pages/post/PostPage.jsx
@@ -1,16 +1,57 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Groups } from "@mui/icons-material";
+import { Avatar, Box, Container, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import { green } from "@mui/material/colors";
+
+function createData(name, item) {
+    return { name, item };
+}
+
+const rows = [
+    createData('phone', '555-555-5555'),
+    createData('email', 'something@gmail.com'),
+    createData('whatsapp', '555-555-5555')
+]
 
 export default function PostPage() {
     return (
        <main>
            <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
-                <Grid container>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <Typography>
-                            Looking for a team to play with
-                        </Typography>
+                <Container>
+                    <Grid container>
+                        <Grid item>
+                            <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+                                <Avatar sx={{ bgcolor: green[400], mr: 2 }} >
+                                    <Groups />
+                                </Avatar> 
+                                <Typography variant="h5">
+                                    Looking for a team to play with 
+                                </Typography>
+                            </Box>
+                            <Typography paragraph>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, exercitationem maxime suscipit corrupti recusandae voluptate sapiente est a. Eos itaque deleniti possimus blanditiis cupiditate alias doloremque rem in quidem? Esse!
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={6}>
+                            <TableContainer component={Paper}>
+                                <Table aria-label="simple table">
+                                    <TableBody>
+                                        {rows.map((row) => (
+                                            <TableRow
+                                                key={row.name}
+                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                            >
+                                                <TableCell component="th" scope="row">
+                                                    {row.name}
+                                                </TableCell>
+                                                <TableCell align="right">{row.item}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </Grid>
                     </Grid>
-                </Grid>
+                </Container>
            </Box>
        </main> 
     )
