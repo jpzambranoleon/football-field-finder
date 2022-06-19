701bb6997dc1634c10da0cb7dccf6df72779edfb
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-18 21:22:59 -0500

    Created CreatePostPage.jsx

---
+++ src/pages/CreatePostPage.jsx
@@ -0,0 +1,11 @@
+export default function CreatePostPage() {
+    return (
+       <main>
+           <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
+                <Container>
+                    
+                </Container>
+           </Box>
+       </main> 
+    )
+};
