4d7935297096682b787931ef95f0ad8a2cce27a1
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-22 17:59:20 -0500

    updated Login.jsx

--- src/authorization/Login.jsx
+++ src/authorization/Login.jsx
@@ -1,11 +1,11 @@
-import { SportsSoccer } from "@mui/icons-material";
-import { Avatar, Box, Button, Checkbox, Container, CssBaseline, FormControlLabel, Grid, Link, Paper, TextField, Typography } from "@mui/material";
+import { LockOutlined } from "@mui/icons-material";
+import { Avatar, Box, Button, Checkbox, CssBaseline, FormControlLabel, Grid, Link, Paper, TextField, Typography } from "@mui/material";
 
 function Copyright(props) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
             {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">
+            <Link color="inherit" href="https://leonedigitale.com">
                 Leone Digitale
             </Link>{' '}
             {new Date().getFullYear()}
@@ -15,83 +15,97 @@ function Copyright(props) {
   }
 
 export default function Login() {
-    return (
-        <Container component="main" maxWidth="xs">
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+    
+    return ( 
+        <Grid container component="main" sx={{ height: '100vh' }}>
             <CssBaseline />
-            <Box
-                sx={{ 
-                    mt: 5,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center'
+            <Grid
+                item
+                xs={false}
+                sm={4}
+                md={7}
+                sx={{
+                    backgroundImage: 'url(/images/football_coach.png)',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: (t) =>
+                    t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
                 }}
-            >
-                <Avatar sx={{ mb: 3, bgcolor: 'secondary.main', width: 56, height: 56 }} size="large">
-                    <SportsSoccer fontSize="large"/>
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign in to First Squad
-                </Typography>
-            </Box>
-            <Paper>
-                <Container>
-                    <Box
-                        sx={{ 
-                            mt: 3,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center'
-                        }}
-                    >
-                        <Box component="form">
-                            <TextField 
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="username"
-                                label="Username"
-                                name="username"
-                                autoFocus
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                            <FormControlLabel 
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign In
-                            </Button>
-                            <Grid container sx={{ mb: 2 }}>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        Forgot password?
-                                    </Link>
-                                </Grid>
-                                <Grid item>
-                                    <Link href="#" variant="body2">
-                                        {"Don't have an account? Sign Up"}
-                                    </Link>
-                                </Grid>
+            />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <Box
+                    sx={{
+                        my: 8,
+                        mx: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlined />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Login
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
                             </Grid>
-                        </Box>
+                            <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                            </Grid>
+                        </Grid>
+                        <Copyright sx={{ mt: 5 }} />
                     </Box>
-                </Container>
-            </Paper>
-            <Copyright sx={{ mt: 8, mb: 4 }} />
-        </Container>
+                </Box>
+            </Grid>
+        </Grid>
     );
 }
