f365d7c4040b4107d24b68d9349fe23432dfb39e
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-22 18:08:49 -0500

    updated Signup.jsx

--- src/authorization/Signup.jsx
+++ src/authorization/Signup.jsx
@@ -1,56 +1,58 @@
-import { SportsSoccer } from "@mui/icons-material";
+import { LockOutlined, SportsSoccer } from "@mui/icons-material";
 import { Avatar, Box, Button, Checkbox, Container, CssBaseline, FormControlLabel, Grid, Link, Paper, TextField, Typography } from "@mui/material";
 
 export default function Signup() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
     return (
         <Container component="main" maxWidth="xs">
             <CssBaseline />
             <Box
-                sx={{ 
-                    mt: 5,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center'
-                }}
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
             >
-                <Avatar sx={{ mb: 3, bgcolor: 'secondary.main', width: 56, height: 56 }} size="large">
-                    <SportsSoccer fontSize="large"/>
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
-                </Typography>
-            </Box>
-            <Paper>
-                <Container>
-                    <Box
-                        sx={{ 
-                            mt: 3,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center'
-                        }}
-                    >
-                        <Box component="form">
-                            <TextField 
-                                margin="normal"
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlined />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Sign up
+            </Typography>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                autoComplete="given-name"
+                                name="userName"
                                 required
                                 fullWidth
-                                id="username"
-                                label="Username"
-                                name="username"
+                                id="userName"
+                                label="User Name"
                                 autoFocus
                             />
-                            <TextField 
-                                margin="normal"
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
                                 required
                                 fullWidth
                                 id="email"
-                                label="Email"
-                                name="Email"
-                                autoFocus
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
                             />
+                        </Grid>
+                        <Grid item xs={12}>
                             <TextField
-                                margin="normal"
                                 required
                                 fullWidth
                                 name="password"
@@ -59,32 +61,31 @@ export default function Signup() {
                                 id="password"
                                 autoComplete="new-password"
                             />
-                            <Grid item xs={12}>
-                                <FormControlLabel 
-                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                    label="I want to receive notifications and updates via email."
-                                    sx={{ mt: 1 }}
-                                />
-                            </Grid>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign Up
-                            </Button>
-                            <Grid container justifyContent="flex-end">
-                                <Grid item>
-                                    <Link href="#" variant="body2">
-                                        Already have an account? Sign in
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                    </Box>
-                </Container>
-            </Paper>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControlLabel
+                                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                label="I want to receive inspiration, marketing promotions and updates via email."
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
         </Container>
     );
 }
