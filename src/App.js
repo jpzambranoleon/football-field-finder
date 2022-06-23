946ddca364bc59285e06d867414a6ccafc9978e2
ZSabakh <zsabakhtarishvili@atu.edu>
2022-06-24 00:34:36 +0400

    Added state management to forms

--- src/App.js
+++ src/App.js
@@ -9,25 +9,27 @@ import Home from "./pages/home/Home";
 import User from "./pages/user/User";
 import Error404 from "./pages/error404/Error404";
 import CreatePost from "./pages/create/CreatePost";
+import axios from "axios";
 
 function App() {
+  axios.defaults.baseURL = "http://localhost:5200";
   return (
     <div className="App">
       <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/post" element={<PostPage />} />
-          <Route path="/create" element={<CreatePost />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<Signup />} />
-          <Route path="/*" element={<Error404 />} />
-        </Routes>
-      </Router>
-      <Copyright />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/post" element={<PostPage />} />
+            <Route path="/create" element={<CreatePost />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-up" element={<Signup />} />
+            <Route path="/*" element={<Error404 />} />
+          </Routes>
+        </Router>
+        <Copyright />
       </>
     </div>
   );
--- src/pages/home/components/FeedFilter.jsx
+++ src/pages/home/components/FeedFilter.jsx
@@ -1,5 +1,19 @@
 import { Directions } from "@mui/icons-material";
-import { Box, Checkbox, Divider, FormControl, FormControlLabel, FormGroup, IconButton, InputBase, InputLabel, MenuItem, Paper, Select, TextField } from "@mui/material";
+import {
+  Box,
+  Checkbox,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  InputBase,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  TextField,
+} from "@mui/material";
 import { useState } from "react";
 
 const ITEM_HEIGHT = 38;
@@ -14,97 +28,161 @@ const MenuProps = {
 };
 
 const states = [
-    'AL', 'AK', 'AZ', 'AR', 'CA',
-    'CO', 'CT', 'DE', 'FL', 'GA',
-    'HI', 'ID', 'IL', 'IN', 'IA',
-    'KS', 'KY', 'LA', 'ME', 'MD',
-    'MA', 'MI', 'MN', 'MS', 'MO',
-    'MT', 'NE', 'NV', 'NH', 'NJ',
-    'NM', 'NY', 'NC', 'ND', 'OH', 
-    'OK', 'OR', 'PA', 'RI', 'SC', 
-    'SD', 'TN', 'TX', 'UT', 'VT', 
-    'VA', 'WA', 'WV', 'WI', 'WY',
+  "AL",
+  "AK",
+  "AZ",
+  "AR",
+  "CA",
+  "CO",
+  "CT",
+  "DE",
+  "FL",
+  "GA",
+  "HI",
+  "ID",
+  "IL",
+  "IN",
+  "IA",
+  "KS",
+  "KY",
+  "LA",
+  "ME",
+  "MD",
+  "MA",
+  "MI",
+  "MN",
+  "MS",
+  "MO",
+  "MT",
+  "NE",
+  "NV",
+  "NH",
+  "NJ",
+  "NM",
+  "NY",
+  "NC",
+  "ND",
+  "OH",
+  "OK",
+  "OR",
+  "PA",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VT",
+  "VA",
+  "WA",
+  "WV",
+  "WI",
+  "WY",
 ];
 
 const FeedFilter = () => {
-    const [state, setState] = useState([]);
-    const handleChange = (event) => {
-        const {
-            target: { value },
-        } = event;
-        setState(
-            typeof value === 'string' ? value.split(',') : value,
-        );
-    };
+  const [filters, setFilters] = useState({
+    team: true,
+    trainer: true,
+    player: true,
+    state: "",
+    city: "",
+    name: "",
+  });
 
-    return (
-        <Paper
-            component="form"
-            id="filter"
-            sx={{ p: '2px 4px', display: {xs: 'block', md: 'flex'}, alignItems: 'center', mb: 5}}
+  return (
+    <Paper component="form" id="filter" sx={{ p: "2px 4px", display: { xs: "block", md: "flex" }, alignItems: "center", mb: 5 }}>
+      <FormControl component="fieldset" sx={{ ml: 1 }}>
+        <FormGroup aria-label="position" row>
+          <FormControlLabel
+            value="end"
+            control={
+              <Checkbox
+                checked={filters.team}
+                onChange={(e) => {
+                  setFilters({ ...filters, team: e.target.checked });
+                }}
+                color="success"
+              />
+            }
+            label="Team"
+            labelPlacement="end"
+          />
+          <FormControlLabel
+            value="end"
+            control={
+              <Checkbox
+                checked={filters.trainer}
+                onChange={(e) => {
+                  setFilters({ ...filters, trainer: e.target.checked });
+                }}
+                color="success"
+              />
+            }
+            label="Trainer"
+            labelPlacement="end"
+          />
+          <FormControlLabel
+            value="end"
+            control={
+              <Checkbox
+                checked={filters.player}
+                onChange={(e) => {
+                  setFilters({ ...filters, player: e.target.checked });
+                }}
+                color="success"
+              />
+            }
+            label="Player"
+            labelPlacement="end"
+          />
+        </FormGroup>
+      </FormControl>
+      <FormControl sx={{ ml: 1, minWidth: 100 }} size="small">
+        <InputLabel id="simple-select-label">State</InputLabel>
+        <Select
+          labelId="simple-select-label"
+          id="simple-select"
+          defaultValue={states[0]}
+          label="State"
+          MenuProps={MenuProps}
+          onChange={(e) => {
+            setFilters({ ...filters, state: e.target.value });
+          }}
         >
-            <FormControl component="fieldset" sx={{ ml: 1 }}>
-                <FormGroup aria-label="position" row>
-                    <FormControlLabel 
-                        value="end"
-                        control={<Checkbox defaultChecked={true} color="success" />}
-                        label="Team"
-                        labelPlacement="end"
-                    />
-                    <FormControlLabel 
-                        value="end"
-                        control={<Checkbox defaultChecked={true} color="success" />}
-                        label="Trainer"
-                        labelPlacement="end"
-                    />
-                    <FormControlLabel 
-                        value="end"
-                        control={<Checkbox defaultChecked={true} color="success" />}
-                        label="Free Agent"
-                        labelPlacement="end"
-                    />
-                </FormGroup>
-            </FormControl>
-            <FormControl sx={{ ml: 1, minWidth: 100 }} size="small">
-                <InputLabel id="simple-select-label">State</InputLabel>
-                <Select
-                    labelId="simple-select-label"
-                    id="simple-select"
-                    value={state}
-                    label="State"
-                    onChange={handleChange}
-                    MenuProps={MenuProps}
-                >
-                    {states.map((name) => (
-                        <MenuItem
-                            key={name}
-                            value={name}
-                        >
-                            {name}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            <TextField 
-                id="outlined-basic" 
-                label="City" 
-                variant="outlined" 
-                size="small"
-                sx={{ ml: 1, width: 150 }}
-            />
-            <Box sx={{ display: 'flex', alignItems: 'center', flex: 1 }}>
-                <InputBase
-                    sx={{ ml: 1, flex: 1 }}
-                    placeholder="Search"
-                    inputProps={{ 'aria-label': 'search' }}
-                />
-                <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-                <IconButton type="submit" color="primary" sx={{ p: '10px' }} aria-label="directions">
-                    <Directions color="success"/>
-                </IconButton>
-            </Box>
-        </Paper>
-    )
-}
+          {states.map((name) => (
+            <MenuItem key={name} value={name}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <TextField
+        id="outlined-basic"
+        label="City"
+        onChange={(e) => {
+          setFilters({ ...filters, city: e.target.value });
+        }}
+        variant="outlined"
+        size="small"
+        sx={{ ml: 1, width: 150 }}
+      />
+      <Box sx={{ display: "flex", alignItems: "center", flex: 1 }}>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search"
+          onChange={(e) => {
+            setFilters({ ...filters, name: e.target.value });
+          }}
+          inputProps={{ "aria-label": "search" }}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton type="submit" color="primary" sx={{ p: "10px" }} aria-label="directions">
+          <Directions color="success" />
+        </IconButton>
+      </Box>
+    </Paper>
+  );
+};
 
 export default FeedFilter;
