069175ffeda420aa72e3992eb31b3ac03fb66f08
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-05 23:32:53 -0500

    created dummyData.js

---
+++ react/dummyData.js
@@ -0,0 +1,18 @@
+export const Users = [
+    {
+        id: 1,
+        username: "Remy Sharp",
+    },
+    {
+        id: 2,
+        username: "Diana Ayi"
+    },
+    {
+        id: 3,
+        username: "Travis Howard"
+    },
+    {
+        id: 4,
+        username: "Cindy Baker"
+    }
+];
