6fde434c53cb1a5556b5986c90cbf64b383f5433
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-06 19:08:44 -0500

    updated dummyData.js

--- react/dummyData.js
+++ react/dummyData.js
@@ -2,17 +2,60 @@ export const Users = [
     {
         id: 1,
         username: "Remy Sharp",
+        email: "",
     },
     {
         id: 2,
-        username: "Diana Ayi"
+        username: "Diana Ayi",
+        email: "",
     },
     {
         id: 3,
-        username: "Travis Howard"
+        username: "Travis Howard",
+        email: "",
     },
     {
         id: 4,
-        username: "Cindy Baker"
+        username: "Cindy Baker",
+        email: ""
     }
+];
+
+export const Posts = [
+    {
+        id: 1,
+        avatar: "",
+        title: "Looking for players",
+        date: "September 14, 2016",
+        state: "AR",
+        city: "Hot Springs",
+        desc: "Our team is looking for more players. We play every Saturday at Lakeside Park between 3:30 to 6:00 pm.",
+        userId: 1,
+        phoneNum: "555-555-5555",
+        email: "email@gmail.com"
+    },
+    {
+        id: 2,
+        avatar: "",
+        title: "Rondey Lo",
+        date: "August 8, 2016",
+        state: "AR",
+        city: "Little Rock",
+        desc: "I'm looking for a team to play with. If you are in a team or know of a team that is looking for players, please conatact me.",
+        userId: 1,
+        phoneNum: "555-555-5555",
+        email: "email@gmail.com"
+    },
+    {
+        id: 1,
+        avatar: "",
+        title: "Trainer Name",
+        date: "June 5, 2016",
+        state: "AR",
+        city: "Betonville",
+        desc: "I'm a soccer coach and trainer. I offer training sessions every Tuesdays and Thursdays from 5:00 to 6:00 pm.",
+        userId: 1,
+        phoneNum: "555-555-5555",
+        email: "email@gmail.com"
+    },
 ];
