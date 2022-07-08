0b259f109e70dd5f74aea73dbcf0df4035095aea
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-07 20:16:18 -0500

    added post props to PostPage.jsx

--- react/src/pages/post/PostPage.jsx
+++ react/src/pages/post/PostPage.jsx
@@ -3,7 +3,7 @@ import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
 import { green } from "@mui/material/colors";
 import SimpleTable from "./components/SimpleTable";
 
-export default function PostPage() {
+export default function PostPage({ post }) {
     return (
        <main>
            <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
@@ -15,14 +15,11 @@ export default function PostPage() {
                                     <Groups fontSize="large" />
                                 </Avatar> 
                                 <Typography variant="h5" fontWeight="bold">
-                                    Looking for more players to join 
+                                    {post.title}
                                 </Typography>
                             </Box>
                             <Typography variant="body1" paragraph>
-                                We are Team Name FC! We play every Sunday at Lakeside Field Park. 
-                                We are looking for more players to come and join our team.
-                                We are hoping to get enough players to play on the Sunday League Tornament.
-                                We practice on Saturdays from 3:30 to 6:00 pm.
+                                {post.desc}
                             </Typography>
                         </Grid>
                         <SimpleTable />
