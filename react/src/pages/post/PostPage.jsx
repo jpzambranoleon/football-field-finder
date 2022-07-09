14a54b4b214b9701143706a32bbc63db73eb3a96
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-08 23:29:30 -0500

    updated PostPage.jsx

--- react/src/pages/post/PostPage.jsx
+++ react/src/pages/post/PostPage.jsx
@@ -2,30 +2,38 @@ import { Groups } from "@mui/icons-material";
 import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
 import { green } from "@mui/material/colors";
 import SimpleTable from "./components/SimpleTable";
+import { Users } from "../../dummyData";
+import Post from "../../components/Post";
 
-export default function PostPage({ post }) {
+const Page = ({ post }) => {
+    return (
+        <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
+            <Container>
+                <Grid container>
+                    <Grid item>
+                        <Box sx={{ display: 'flex', alignItems: 'center', mb: 5 }}>
+                            <Avatar sx={{ bgcolor: green[400], mr: 2, height: '55px', width: '55px' }} >
+                                <Groups fontSize="large" />
+                            </Avatar> 
+                            <Typography variant="h5" fontWeight="bold">
+                                {post.title}
+                            </Typography>
+                        </Box>
+                        <Typography variant="body1" paragraph>
+                            {post.desc}
+                        </Typography>
+                    </Grid>
+                    <SimpleTable />
+                </Grid>
+            </Container>
+        </Box>
+    )
+}
+
+export default function PostPage() {
     return (
        <main>
-           <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
-                <Container>
-                    <Grid container>
-                        <Grid item>
-                            <Box sx={{ display: 'flex', alignItems: 'center', mb: 5 }}>
-                                <Avatar sx={{ bgcolor: green[400], mr: 2, height: '55px', width: '55px' }} >
-                                    <Groups fontSize="large" />
-                                </Avatar> 
-                                <Typography variant="h5" fontWeight="bold">
-                                    {post.title}
-                                </Typography>
-                            </Box>
-                            <Typography variant="body1" paragraph>
-                                {post.desc}
-                            </Typography>
-                        </Grid>
-                        <SimpleTable />
-                    </Grid>
-                </Container>
-           </Box>
+           <Page />
        </main> 
     )
 };
