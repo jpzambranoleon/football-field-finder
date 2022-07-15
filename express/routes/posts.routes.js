f0e443daae0dfd85975e94c46ce978aa4811d45f
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-15 00:08:48 -0500

    added delete router to posts.routes.js

--- express/routes/posts.routes.js
+++ express/routes/posts.routes.js
@@ -27,6 +27,19 @@ router.put("/:id", async(req, res) => {
     }
 });
 // delete a post
+router.delete("/:id", async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.deleteOne({ $set:req.body });
+            res.status(200).json("the post has been deleted")
+        } else {
+            res.status(403).json("you can delete only your post")
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 // get a post
 // get timeline posts
 
