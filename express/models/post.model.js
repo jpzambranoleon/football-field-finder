246caa6b8b347ca2c499b87ff147a993132b5371
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-14 20:35:54 -0500

    created post.models.js

---
+++ express/models/post.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        desc: {
+            type: String,
+            max: 500,
+        }
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model("Post", postSchema);
