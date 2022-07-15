8f1e102f2a562085af61c59b039c09d75a5cab8c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-14 20:30:40 -0500

    Created a router to get user info

--- express/routes/users.routes.js
+++ express/routes/users.routes.js
@@ -4,7 +4,7 @@ const bycrypt = require("bcryptjs");
 
 // update user
 router.put("/:id", async(req, res) => {
-    if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 const salt = await bycrypt.genSalt(10);
@@ -15,15 +15,38 @@ router.put("/:id", async(req, res) => {
         }
         try {
             const user = await User.findByIdAndUpdate(req.params.id, {$set:req.body});
-            res.status(200).json("Acount has been updated")
+            res.status(200).json("Acount has been updated");
         } catch (err) {
            return res.status.apply(500).json(err); 
         }
     } else {
         return res.status(403).json("You can update only your account!");
     }
-})
+});
+
 // delete user
+router.delete("/:id", async(req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Acount has been deleted");
+        } catch (err) {
+           return res.status.apply(500).json(err); 
+        }
+    } else {
+        return res.status(403).json("You can delete only your account!");
+    }
+});
+
 // get a user
+router.get("/:id", async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const {password, updatedAt, ...other} = user._doc
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
