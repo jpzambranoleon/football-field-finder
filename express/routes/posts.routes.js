b43e114c05e3e209ff88a2e2e2f5a5bc30224b8c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-14 23:29:50 -0500

    added update a post router

--- express/routes/posts.routes.js
+++ express/routes/posts.routes.js
@@ -10,8 +10,22 @@ router.post("/", async(req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
 // update a post
+router.put("/:id", async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({ $set:req.body });
+            res.status(200).json("the post has been updated")
+        } else {
+            res.status(403).json("you can updated only your post")
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 // delete a post
 // get a post
 // get timeline posts
