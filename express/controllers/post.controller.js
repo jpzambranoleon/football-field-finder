3fc23c656e2ab8e5092e6b2196cd1cc6236e586d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-16 01:07:20 -0500

    created delete post routes and controllers

--- express/controllers/post.controller.js
+++ express/controllers/post.controller.js
@@ -26,3 +26,18 @@ exports.UpdatePost = async (req, res) => {
     res.status(500).json(err);
   }
 };
+
+// DELETE A POST
+exports.DeletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- express/routes/posts.routes.js
+++ express/routes/posts.routes.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const PostController = require("../controllers/post.controller");
 
 router.post("/", PostController.Submit);
+router.put("/:id", PostController.UpdatePost);
+router.delete("/:id", PostController.DeletePost);
 
 module.exports = router;
