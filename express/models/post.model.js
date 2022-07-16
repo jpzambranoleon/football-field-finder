c9edb84c24e3b33c73de2d081726bb39815a5d2d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-15 19:28:48 -0500

    updated post.models.js

--- express/models/post.model.js
+++ express/models/post.model.js
@@ -1,34 +1,46 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const postSchema = new mongoose.Schema(
-    {
-        userId: {
-            type: String,
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        desc: {
-            type: String,
-            max: 500,
-        },
-        city: {
-            type: String,
-            max: 50,
-        },
-        cellPhone: {
-            type: String,
-            max: 20,
-        },
-        email: {
-            type: String,
-            max: 50,
-            unique: true,
-        }
+const postSchema = new Schema(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "Invalid User"],
     },
-    {timestamps: true}
+    type: {
+      type: Schema.Types.ObjectId,
+      ref: "post_type",
+      required: [true, "Invalid post tyle"],
+    },
+    title: { type: String, required: true },
+    desc: { type: String, required: true },
+    state: {
+      type: Schema.Types.ObjectId,
+      ref: "state",
+      required: [true, "Invalid state"],
+    },
+    city: {
+      type: Schema.Types.ObjectId,
+      ref: "city",
+      required: [true, "Invalid city"],
+    },
+    contact: {
+      phone: { value: String, public: { type: Boolean, default: true } },
+      email: { value: String, public: { type: Boolean, default: true } },
+    },
+    location: {
+      description: String,
+      place_id: String,
+      lat: Number,
+      lng: Number,
+    },
+    active: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model("Post", postSchema);
+const Post = mongoose.model("post", postSchema);
+module.exports = Post;
