7b27ff9bf5ca9b0b7a6b7b8d4ce08904c799d4a7
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-15 21:16:03 -0500

    updated files to work with postman and mongodb

--- express/app.js
+++ express/app.js
@@ -3,9 +3,6 @@ const cors = require("cors");
 const helmet = require("helmet");
 const MongooseConnection = require("./utility/mongoose.connection");
 const morgan = require("morgan");
-const userRoute = require("./routes/users.routes");
-const authRoute = require("./routes/auth.routes");
-const postRoute = require("./routes/posts.routes");
 
 // connect to database
 require("dotenv").config();
@@ -20,9 +17,7 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use(cors());
 
-app.use("/api/users", userRoute);
-app.use("/api/auth", authRoute);
-app.use("/api/posts", postRoute);
+app.use("/api/auth", require("./routes/auth.routes"));
 
 app.listen(5200, () => {
   console.log("Server started listening on PORT : " + 5200);
--- express/controllers/user.controller.js
+++ express/controllers/user.controller.js
@@ -3,7 +3,7 @@ const User = require("../models/user.model");
 exports.Register = async (req, res) => {
   try {
     // generate hashed password
-    const hash = await User.hashPassword(req.body.hashPassword);
+    const hash = await User.hashPassword(req.body.password);
     req.body.password = hash;
 
     // create new user
@@ -13,6 +13,32 @@ exports.Register = async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
-    res.status(500).json(err);
+    if (error.name === "ValidationError") {
+      res.status(400).send({ error: true, message: error.message });
+      return;
+    }
+    res.status(500).send({
+      error: true,
+      message: error.message,
+    });
+  }
+};
+
+exports.Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
