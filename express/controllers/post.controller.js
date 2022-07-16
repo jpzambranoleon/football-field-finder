d2802dd9a4fc378a2ac89a9cb7e7d8cbb527a183
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-16 00:06:15 -0500

    updated file

--- express/controllers/post.controller.js
+++ express/controllers/post.controller.js
@@ -1,2 +1,13 @@
 const Post = require("../models/post.model");
 const City = require("../models/city.models");
+
+// CREATE A POST
+exports.Submit = async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
