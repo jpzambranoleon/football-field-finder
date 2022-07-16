d52740a19f302d5c3a73bdd2fc0ce26468d1c71d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-16 00:44:02 -0500

    created update post async function

--- express/controllers/post.controller.js
+++ express/controllers/post.controller.js
@@ -11,3 +11,18 @@ exports.Submit = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+// UPDATE A POST
+exports.UpdatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
