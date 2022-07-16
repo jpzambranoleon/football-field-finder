e3dc6d749f2770848949c5eb90b9d50c8e306335
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-16 00:06:36 -0500

    updated file

--- express/routes/posts.routes.js
+++ express/routes/posts.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const PostController = require("../controllers/post.controller");
 
-router.post("/", PostController.Post);
+router.post("/submit", PostController.Submit);
 
 module.exports = router;
