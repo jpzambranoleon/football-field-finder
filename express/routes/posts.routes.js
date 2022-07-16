1e56d950329d1b1322654e15e09731f0bab2ecab
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-15 23:53:59 -0500

    added router.post

--- express/routes/posts.routes.js
+++ express/routes/posts.routes.js
@@ -1,65 +1,6 @@
 const router = require("express").Router();
-const Post = require("../models/post.model");
+const PostController = require("../controllers/post.controller");
 
-// create a post
-router.post("/", async (req, res) => {
-  const newPost = new Post(req.body);
-  try {
-    const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// update a post
-router.put("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
-      await post.updateOne({ $set: req.body });
-      res.status(200).json("the post has been updated");
-    } else {
-      res.status(403).json("you can updated only your post");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// delete a post
-router.delete("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
-      await post.deleteOne({ $set: req.body });
-      res.status(200).json("the post has been deleted");
-    } else {
-      res.status(403).json("you can delete only your post");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// get a post
-router.get("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// get timeline posts
-router.get("/timeline", async (req, res) => {
-  try {
-    const currentUser = await User.findById(req.body.userId);
-    const userPosts = await Post.find({ userId: currentUser._id });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.post("/", PostController.Post);
 
 module.exports = router;
