09e3171e89da34cd53fffa755169b04643887df8
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-16 01:09:43 -0500

    created UpdateUser async function to user.controllers.js

--- express/controllers/user.controller.js
+++ express/controllers/user.controller.js
@@ -1,5 +1,8 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
+const {
+  ExportConfigurationInstance,
+} = require("twilio/lib/rest/bulkexports/v1/exportConfiguration");
 
 // REGISTER
 exports.Register = async (req, res) => {
@@ -41,5 +44,29 @@ exports.Login = async (req, res) => {
       error: true,
       message: error.message,
     });
+  }
+};
+
+// Update User
+exports.UpdateUser = async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account!");
   }
 };
