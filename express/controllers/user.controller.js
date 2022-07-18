f59b07e84835bcc65f206084bf399e1d4c15ec80
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-17 21:15:55 -0500

    created a delete user for user.controller.js

--- express/controllers/user.controller.js
+++ express/controllers/user.controller.js
@@ -47,7 +47,7 @@ exports.Login = async (req, res) => {
   }
 };
 
-// Update User
+// UPDATE USER
 exports.UpdateUser = async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
@@ -68,5 +68,19 @@ exports.UpdateUser = async (req, res) => {
     }
   } else {
     return res.status(403).json("You can update only your account!");
+  }
+};
+
+// DELETE USER
+exports.DeleteUser = async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!");
   }
 };
