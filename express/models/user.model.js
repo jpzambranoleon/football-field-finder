4056750441b53158c4c62f3f5f6d3915322d89d1
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-18 22:14:55 -0500

    updated user.model.js

--- express/models/user.model.js
+++ express/models/user.model.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema(
   {
-    username: { type: String, required: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
