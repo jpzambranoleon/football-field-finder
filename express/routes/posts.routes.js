2400dce072319b1aeff656c51ed713e10cd711c1
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-19 23:43:46 -0500

    labled the router functions in posts.routes.js

--- express/routes/posts.routes.js
+++ express/routes/posts.routes.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const PostController = require("../controllers/post.controller");
 
+// Submit a Post
 router.post("/", PostController.Submit);
+// Update a Post
 router.put("/:id", PostController.UpdatePost);
+// Delete a Post
 router.delete("/:id", PostController.DeletePost);
 
 module.exports = router;
