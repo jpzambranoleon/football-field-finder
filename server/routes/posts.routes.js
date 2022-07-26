3aa88f53184804694b1e6031d820857f6139af27
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-26 01:02:56 -0500

    created posts.routes.js

---
+++ server/routes/posts.routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const PostController = require("../controllers/post.controller");
+
+// Submit a Post
+router.post("/", PostController.Submit);
+// Update a Post
+router.put("/:id", PostController.UpdatePost);
+// Delete a Post
+router.delete("/:id", PostController.DeletePost);
+// Get a Post
+router.get("/:id", PostController.GetPost);
+// Get Timeline of User Posts
+router.get("/userposts/:userId", PostController.GetUserPosts);
+
+module.exports = router;
