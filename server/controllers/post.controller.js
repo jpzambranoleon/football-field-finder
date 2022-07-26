4463141ae093b66925bf41780c36bb018f1e4daf
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-26 01:08:33 -0500

    created controllers for post and user

---
+++ server/controllers/post.controller.js
@@ -0,0 +1,65 @@
+const Post = require("../models/post.model");
+const User = require("../models/user.model");
+
+// CREATE A POST
+exports.Submit = async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// UPDATE A POST
+exports.UpdatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// DELETE A POST
+exports.DeletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// GET A POST
+exports.GetPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// GET USER POSTS
+exports.GetUserPosts = async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.params.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+
+    res.status(200).json(userPosts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
---
+++ server/controllers/user.controller.js
@@ -0,0 +1,86 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const {
+  ExportConfigurationInstance,
+} = require("twilio/lib/rest/bulkexports/v1/exportConfiguration");
+
+// REGISTER
+exports.Register = async (req, res) => {
+  try {
+    // generate hashed password
+    const hash = await User.hashPassword(req.body.password);
+    req.body.password = hash;
+
+    // create new user
+    const newUser = new User(req.body);
+
+    // save user and response
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      res.status(400).send({ error: true, message: error.message });
+      return;
+    }
+    res.status(500).send({
+      error: true,
+      message: error.message,
+    });
+  }
+};
+
+// LOGIN
+exports.Login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = bcrypt.compareSync(req.body.password, user.password);
+    if (!validPassword) throw new Error("Invalid credentials");
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send({
+      error: true,
+      message: error.message,
+    });
+  }
+};
+
+// UPDATE USER
+exports.UpdateUser = async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account!");
+  }
+};
+
+// DELETE USER
+exports.DeleteUser = async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!");
+  }
+};
