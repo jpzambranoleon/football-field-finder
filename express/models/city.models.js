c464c35f1a8413c5bbb5740a0a6f1d0cbefe4614
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-26 01:51:28 -0500

    deleted old files

--- express/models/city.models.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const citySchema = new Schema({
-  name: { type: String, required: true, unique: true },
-});
-
-const City = mongoose.model("city", citySchema);
-module.exports = City;
--- express/routes/users.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const UserController = require("../controllers/user.controller");
-
-// UPDATE USER
-router.put("/:id", UserController.UpdateUser);
-// DELETE USER
-router.delete("/:id", UserController.DeleteUser);
-
-module.exports = router;
