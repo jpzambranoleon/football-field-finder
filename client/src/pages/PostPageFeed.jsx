52982678786c47b2e60ba5982a01e1299d55a14f
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-27 01:29:55 -0500

    updated PostPageFeed.jsx

--- client/src/pages/PostPageFeed.jsx
+++ client/src/pages/PostPageFeed.jsx
@@ -2,8 +2,6 @@ import { Groups } from "@mui/icons-material";
 import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
 import { green } from "@mui/material/colors";
 import SimpleTable from "../components/SimpleTable";
-import { Users } from "../dummyData";
-import { Posts } from "../dummyData";
 
 export default function PostPageFeed() {
   return (
