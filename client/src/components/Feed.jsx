0fd4c63b8b26b391471c9f5507beba62bdee3a12
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-28 16:38:34 -0500

    updated Feed.jsx

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -2,18 +2,18 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Post from "./Post";
 
-const Feed = ({ name }) => {
+const Feed = ({ username }) => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = name
-        ? await axios.get("posts/myposts/" + name)
+      const res = username
+        ? await axios.get("posts/myposts/" + username)
         : await axios.get("posts/myposts/");
       setPosts(res.data);
     };
     fetchPosts();
-  }, [name]);
+  }, [username]);
 
   return (
     <>
