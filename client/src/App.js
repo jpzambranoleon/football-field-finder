4545bff544be81b10abd1a02bb367ec82c04b037
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-28 01:07:08 -0500

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,11 @@ import Login from "./authorization/Login";
 import Signup from "./authorization/Signup";
 import Copyright from "./components/Copyright";
 import Home from "./pages/Home";
-import User from "./pages/User";
 import Error404 from "./pages/Error404";
 import CreatePost from "./pages/CreatePost";
 import axios from "axios";
 import PostPageFeed from "./pages/PostPageFeed";
+import MyPosts from "./pages/MyPosts";
 
 function App() {
   axios.defaults.baseURL = "http://localhost:8800/api";
@@ -17,11 +17,11 @@ function App() {
     <div className="App">
       <>
         <Router>
-          <Navbar />
+          <Navbar userId="62df98e664eb28ada61acfcb" />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/user" element={<User />} />
+            <Route path="/myposts/:name" element={<MyPosts />} />
             <Route path="/post" element={<PostPageFeed />} />
             <Route path="/create" element={<CreatePost />} />
             <Route path="/login" element={<Login />} />
