3d5a7cd12e765d1ea0596717d3d01e54563f9939
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-29 00:00:21 -0500

    add InfoProvider ContextApi

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { InfoProvider } from "./utils/InfoProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <InfoProvider>
+      <App />
+    </InfoProvider>
   </React.StrictMode>
 );
 
