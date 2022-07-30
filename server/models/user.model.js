723c257a7baf8255019dc9e82c2e0cdcba301b98
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-29 23:01:10 -0500

    updated userSchema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,8 +5,8 @@ const bcrypt = require("bcryptjs");
 const userSchema = new Schema(
   {
     username: { type: String, required: true },
-    firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
