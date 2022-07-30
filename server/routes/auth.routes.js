236d9e651dae0a5a204cc647664f980f7c03ff08
Jean-Paul <jpzambrano.leon@gmail.com>
2022-07-29 23:00:35 -0500

    added GetProfileData from AuthController

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const AuthController = require("../controllers/user.controller");
 const { checkDuplicateEmail } = require("../middlewares/register.middleware");
+const { validateToken } = require("../middlewares/validate.jwt.middleware");
 
 // Register
 router.post("/register", [checkDuplicateEmail], AuthController.Register);
 // Login
 router.post("/login", AuthController.Login);
+// Get Profile Data
+router.get("/profile", validateToken, AuthController.GetProfileData);
 
 module.exports = router;
