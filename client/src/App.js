ab59a22f5a114d107b0d5acaefbf085e36e4ac0d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-01 17:05:27 -0500

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,14 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
-import About from "./pages/About";
+import About from "./pages/About/About";
 import Login from "./authorization/Login";
 import Register from "./authorization/Register";
-import Home from "./pages/Home";
-import CreatePost from "./pages/CreatePost";
+import Home from "./pages/Home/Home";
+import CreatePost from "./pages/CreatePost/CreatePost";
 import axios from "axios";
-import PostPageFeed from "./pages/PostPageFeed";
-import Profile from "./pages/Profile";
+import ViewPost from "./pages/ViewPost/ViewPost";
+import Profile from "./pages/Profile/Profile";
 import { useContext } from "react";
 import { InfoContext } from "./utils/InfoProvider";
 import Navbar from "./components/Navbar";
@@ -30,7 +30,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/post/view/:postId" element={<PostPageFeed />} />
+            <Route path="/post/view/:postId" element={<ViewPost />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             {authorized ? (
