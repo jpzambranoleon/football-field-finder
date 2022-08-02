d51686df49ea6e37e71ee9760a9d04379bd53027
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-01 23:59:59 -0500

    updated ViewPost.jsx

--- client/src/pages/ViewPost/ViewPost.jsx
+++ client/src/pages/ViewPost/ViewPost.jsx
@@ -1,6 +1,6 @@
-import { Groups } from "@mui/icons-material";
+import { Groups, Person, Sports } from "@mui/icons-material";
 import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
-import { green } from "@mui/material/colors";
+import { blue, green, pink } from "@mui/material/colors";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -8,16 +8,51 @@ import SimpleTable from "./components/SimpleTable";
 
 export default function ViewPost() {
   const [post, setPost] = useState({});
+  const [postType, setPostType] = useState();
   const postId = useParams().postId;
 
   useEffect(() => {
     const fetchPost = async () => {
       const res = await axios.get(`/posts/${postId}`);
       setPost(res.data);
-      console.log(res.data);
+      if (res.data.types.team === true) {
+        return setPostType("Team");
+      } else if (res.data.types.player === true) {
+        return setPostType("Player");
+      } else if (res.data.types.coach === true) {
+        return setPostType("Coach");
+      }
     };
     fetchPost();
   }, [postId]);
+
+  const PostIcon = () => {
+    if (postType === "Team") {
+      return (
+        <Avatar
+          sx={{ bgcolor: green[400], mr: 2, height: "55px", width: "55px" }}
+        >
+          <Groups fontSize="large" />
+        </Avatar>
+      );
+    } else if (postType === "Player") {
+      return (
+        <Avatar
+          sx={{ bgcolor: blue[400], mr: 2, height: "55px", width: "55px" }}
+        >
+          <Person fontSize="large" />
+        </Avatar>
+      );
+    } else if (postType === "Coach") {
+      return (
+        <Avatar
+          sx={{ bgcolor: pink[400], mr: 2, height: "55px", width: "55px" }}
+        >
+          <Sports fontSize="large" />
+        </Avatar>
+      );
+    }
+  };
 
   return (
     <main>
@@ -26,16 +61,7 @@ export default function ViewPost() {
           <Grid container>
             <Grid item xs={12}>
               <Box sx={{ display: "flex", alignItems: "center", mb: 5 }}>
-                <Avatar
-                  sx={{
-                    bgcolor: green[400],
-                    mr: 2,
-                    height: "55px",
-                    width: "55px",
-                  }}
-                >
-                  <Groups fontSize="large" />
-                </Avatar>
+                <PostIcon />
                 <Typography variant="h5" fontWeight="bold">
                   {post.title}
                 </Typography>
@@ -45,7 +71,7 @@ export default function ViewPost() {
               <Typography variant="body1" paragraph>
                 {post.desc}
               </Typography>
-              <SimpleTable post={post} />
+              <SimpleTable post={post} postType={postType} />
             </Grid>
           </Grid>
         </Container>
