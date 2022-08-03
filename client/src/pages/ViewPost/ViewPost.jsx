d09f6df711049ff3e91fa7ddca13caef8c735f1f
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-03 02:39:38 -0500

    updated ViewPost.jsx

--- client/src/pages/ViewPost/ViewPost.jsx
+++ client/src/pages/ViewPost/ViewPost.jsx
@@ -1,13 +1,25 @@
 import { Groups, Person, Sports } from "@mui/icons-material";
-import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { blue, green, pink } from "@mui/material/colors";
 import axios from "axios";
+import { useContext } from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { InfoContext } from "../../utils/InfoProvider";
+import DeletePostButton from "./components/DeletePostButton";
 import SimpleTable from "./components/SimpleTable";
 
 export default function ViewPost() {
   const [post, setPost] = useState({});
+  const { authorizedUser } = useContext(InfoContext);
   const [postType, setPostType] = useState();
   const postId = useParams().postId;
 
@@ -74,6 +86,12 @@ export default function ViewPost() {
               <SimpleTable post={post} postType={postType} />
             </Grid>
           </Grid>
+          {authorizedUser._id !== post.userId ? null : (
+            <Stack spacing={2} direction="row" sx={{ mt: 2 }}>
+              <Button color="success">Edit</Button>
+              <DeletePostButton post={post} />
+            </Stack>
+          )}
         </Container>
       </Box>
     </main>
