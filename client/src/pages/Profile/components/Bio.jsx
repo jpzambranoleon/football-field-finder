0de337d38c02dc355ec6432b5e79095abe6a91d2
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-03 02:39:28 -0500

    updated Bio.jsx

--- client/src/pages/Profile/components/Bio.jsx
+++ client/src/pages/Profile/components/Bio.jsx
@@ -1,38 +1,37 @@
-import { Avatar, Box, Grid, Paper, Typography } from "@mui/material";
-import Feed from "./Feed";
+import { Delete, Edit } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  IconButton,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 const Bio = ({ authorizedUser }) => {
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12} sm={3}>
-        <Paper>
-          <Box sx={{ justifyContent: "center", display: "flex" }}>
-            <Avatar sx={{ width: 115, height: 115 }} />
-          </Box>
-          <Typography align="center">
-            {authorizedUser.firstName} {authorizedUser.lastName}
-          </Typography>
-          <Typography align="center"> Email: {authorizedUser.email}</Typography>
-        </Paper>
-      </Grid>
-      <Grid item xs={12} sm={9}>
-        <Paper>
-          <Box p={2}>
-            <Typography
-              variant="h6"
-              align="center"
-              color="text.secondary"
-              paragraph
-            >
-              My Posts
-            </Typography>
-            <Grid container spacing={2}>
-              <Feed username={authorizedUser.username} />
-            </Grid>
-          </Box>
-        </Paper>
-      </Grid>
-    </Grid>
+    <Paper>
+      <Box p={3}>
+        <Box sx={{ justifyContent: "center", display: "flex", mb: 2 }}>
+          <Avatar sx={{ width: 115, height: 115 }} />
+        </Box>
+        <Typography align="center" paragraph>
+          {authorizedUser.firstName} {authorizedUser.lastName}
+        </Typography>
+        <Typography align="center">
+          Username: {authorizedUser.username}
+        </Typography>
+        <Typography align="center"> Email: {authorizedUser.email}</Typography>
+        <Stack spacing={2} direction="row" sx={{ mt: 3 }}>
+          <IconButton color="success">
+            <Edit />
+          </IconButton>
+          <IconButton color="error">
+            <Delete />
+          </IconButton>
+        </Stack>
+      </Box>
+    </Paper>
   );
 };
 
