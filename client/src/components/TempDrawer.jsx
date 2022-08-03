56d8626f3737081364ecc9e91a23f7050ac94b52
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-02 20:24:59 -0500

    updated TempDrawer to properly navigate

--- client/src/components/TempDrawer.jsx
+++ client/src/components/TempDrawer.jsx
@@ -1,58 +1,79 @@
-import { Box, ClickAwayListener, Drawer, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
+import {
+  Box,
+  ClickAwayListener,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { InfoContext } from "../utils/InfoProvider";
 
 const TempDrawer = ({ openDrawer, setOpenDrawer }) => {
-    const handleDrawerClose = () => {
-        setOpenDrawer(false);
-    };
+  const { authorized, authorizedUser } = useContext(InfoContext);
 
-    return (
-        <Drawer anchor="left" open={openDrawer} sx={{ display: { sm: 'none' } }} >
-            <ClickAwayListener onClickAway={handleDrawerClose}>
-                <Box sx={{ width: 250, height: '100%' }}>
-                    <List>
-                        <ListItem>
-                            <Typography 
-                                component="h3"
-                                variant="h5"
-                                color="success.main"
-                                gutterBottom
-                            >
-                                Team Finder
-                            </Typography>
-                        </ListItem>
-                        <ListItem disablePadding>
-                            <ListItemButton onClick={handleDrawerClose} component={Link} to="/">
-                                <ListItemText>
-                                    <Typography>
-                                        Home
-                                    </Typography>
-                                </ListItemText>
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem disablePadding>
-                            <ListItemButton onClick={handleDrawerClose} component={Link} to="/about">
-                                <ListItemText>
-                                    <Typography>
-                                        About
-                                    </Typography>
-                                </ListItemText>
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem disablePadding>
-                            <ListItemButton onClick={handleDrawerClose} component={Link} to="/user">
-                                <ListItemText>
-                                    <Typography>
-                                        My Posts
-                                    </Typography>
-                                </ListItemText>
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                </Box>  
-            </ClickAwayListener> 
-        </Drawer>
-    )
+  const handleDrawerClose = () => {
+    setOpenDrawer(false);
+  };
+
+  return (
+    <Drawer anchor="left" open={openDrawer} sx={{ display: { sm: "none" } }}>
+      <ClickAwayListener onClickAway={handleDrawerClose}>
+        <Box sx={{ width: 250, height: "100%" }}>
+          <List>
+            <ListItem>
+              <Typography
+                component="h3"
+                variant="h5"
+                color="success.main"
+                gutterBottom
+              >
+                Team Finder
+              </Typography>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton
+                onClick={handleDrawerClose}
+                component={Link}
+                to="/"
+              >
+                <ListItemText>
+                  <Typography>Home</Typography>
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton
+                onClick={handleDrawerClose}
+                component={Link}
+                to="/about"
+              >
+                <ListItemText>
+                  <Typography>About</Typography>
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton
+                onClick={handleDrawerClose}
+                component={Link}
+                to={
+                  authorized ? `profile/${authorizedUser.username}` : "/login"
+                }
+              >
+                <ListItemText>
+                  <Typography>{authorized ? "Profile" : "Login"}</Typography>
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </ClickAwayListener>
+    </Drawer>
+  );
 };
 
-export default TempDrawer
+export default TempDrawer;
