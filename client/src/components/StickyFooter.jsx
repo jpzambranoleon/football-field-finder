ec11b7f383b2dc0f1e0e2da2c87e5187194ecc61
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-03 17:39:51 -0500

    created StickyFooter.jsx

---
+++ client/src/components/StickyFooter.jsx
@@ -0,0 +1,37 @@
+import { Box, Container, Link, Typography } from "@mui/material";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://leonedigitale.com/">
+        Leone Digitale
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const StickyFooter = () => {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        px: 2,
+        mt: "auto",
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+      }}
+    >
+      <Container maxWidth="sm">
+        <Copyright />
+      </Container>
+    </Box>
+  );
+};
+
+export default StickyFooter;
