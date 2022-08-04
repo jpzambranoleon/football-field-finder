ecc7c40c327c96cb66419dcbdec365a127b8e3ca
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-04 00:00:51 -0500

    updated routes

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -4,11 +4,11 @@ const PostController = require("../controllers/post.controller");
 // Submit a Post
 router.post("/submit", PostController.SubmitPost);
 // Update a Post
-router.put("/:id", PostController.UpdatePost);
+router.put("/update/:id", PostController.UpdatePost);
 // Delete a Post
 router.delete("/delete/:id", PostController.DeletePost);
 // Get a Post
-router.get("/:id", PostController.GetPost);
+router.get("/get/:id", PostController.GetPost);
 
 // Get Timeline of User Posts
 router.get("/profile/:username", PostController.GetUserPostsAll);
