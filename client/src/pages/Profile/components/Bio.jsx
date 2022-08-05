e2626f0e51a16a1d77aaf8b917b6c299bb586311
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-04 23:14:04 -0500

    updated Bio.jsx

--- client/src/pages/Profile/components/Bio.jsx
+++ client/src/pages/Profile/components/Bio.jsx
@@ -1,24 +1,27 @@
-import { Avatar, Box, Button, Paper, Typography } from "@mui/material";
+import { Avatar, Box, Button, Typography } from "@mui/material";
 
 const Bio = ({ authorizedUser }) => {
   return (
-    <Paper>
-      <Box p={3}>
-        <Box sx={{ justifyContent: "center", display: "flex", mb: 2 }}>
-          <Avatar sx={{ width: 115, height: 115 }} />
-        </Box>
-        <Typography align="center" paragraph>
-          {authorizedUser.firstName} {authorizedUser.lastName}
-        </Typography>
-        <Typography align="center">
-          Username: {authorizedUser.username}
-        </Typography>
-        <Typography align="center"> Email: {authorizedUser.email}</Typography>
-        <Button fullWidth color="success" variant="outlined" sx={{ mt: 3 }}>
-          Edit Profile
-        </Button>
+    <Box>
+      <Box sx={{ mb: 2 }}>
+        <Avatar sx={{ width: 270, height: 270 }} />
       </Box>
-    </Paper>
+      <Typography variant="h1" fontSize="22px" fontWeight={600}>
+        {authorizedUser.firstName} {authorizedUser.lastName}
+      </Typography>
+      <Typography
+        variant="body1"
+        fontSize="20px"
+        color="text.secondary"
+        sx={{ mb: 2 }}
+      >
+        {authorizedUser.username}
+      </Typography>
+      <Typography> Email: {authorizedUser.email}</Typography>
+      <Button fullWidth color="success" variant="outlined" sx={{ mt: 2 }}>
+        Edit Profile
+      </Button>
+    </Box>
   );
 };
 
