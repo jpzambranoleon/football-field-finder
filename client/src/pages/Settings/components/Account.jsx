b8ddbb340273f3bb70d804ebcbeae70fc89cd99b
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-06 02:17:51 -0500

    created Account.jsx

---
+++ client/src/pages/Settings/components/Account.jsx
@@ -0,0 +1,45 @@
+import { Box, Button, Typography } from "@mui/material";
+
+const Account = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+          mb: 2,
+        }}
+      >
+        <Typography variant="h5" gutterBottom>
+          Change username
+        </Typography>
+      </Box>
+      <Button variant="contained" size="small" sx={{ textTransform: "none" }}>
+        Change username
+      </Button>
+      <Box sx={{ borderBottom: 1, borderColor: "divider", mb: 2 }}>
+        <Typography variant="h5" gutterBottom>
+          Change password
+        </Typography>
+      </Box>
+      <Button variant="contained" size="small" sx={{ textTransform: "none" }}>
+        Change password
+      </Button>
+      <Box sx={{ borderBottom: 1, borderColor: "divider", mb: 2 }}>
+        <Typography variant="h5" gutterBottom color="error">
+          Delete User
+        </Typography>
+      </Box>
+      <Button
+        color="error"
+        variant="contained"
+        size="small"
+        sx={{ textTransform: "none" }}
+      >
+        Delete user
+      </Button>
+    </>
+  );
+};
+
+export default Account;
