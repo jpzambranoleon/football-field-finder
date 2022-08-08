800794d9a5691eed969b8453c6b929784f1137e5
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-07 23:34:36 -0500

    added get_all route

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -2,18 +2,21 @@ const router = require("express").Router();
 const PostController = require("../controllers/post.controller");
 
 // Submit a Post
-router.post("/submit", PostController.SubmitPost);
+router.post("/submit", PostController.submitPost);
 // Update a Post
-router.put("/update/:id", PostController.UpdatePost);
+router.put("/update/:id", PostController.updatePost);
 // Delete a Post
-router.delete("/delete/:id", PostController.DeletePost);
+router.delete("/delete/:id", PostController.deletePost);
+
+router.get("/get_all", PostController.getAll);
+
 // Get a Post
-router.get("/get/:id", PostController.GetPost);
+router.get("/get/:id", PostController.getPost);
 
 // Get Timeline of User Posts
-router.get("/profile/:username", PostController.GetUserPostsAll);
+router.get("/profile/:username", PostController.getUserPostsAll);
 
 // Get Timeline of All Posts
-router.get("/timeline/all", PostController.GetTimelinePosts);
+router.get("/timeline/all", PostController.getTimelinePosts);
 
 module.exports = router;
