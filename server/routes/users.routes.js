758f63299d67b49fe3384481f4e85a5b431227b0
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-10 01:58:09 -0500

    added router

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const UserController = require("../controllers/user.controller");
+const { validateToken } = require("../middlewares/validate.jwt.middleware");
 
 // UPDATE USER
 router.put("/:id", UserController.UpdateUser);
@@ -7,5 +8,7 @@ router.put("/:id", UserController.UpdateUser);
 router.delete("/:id", UserController.DeleteUser);
 // GET USER
 router.get("/", UserController.GetUser);
+
+router.get("/profile", validateToken, UserController.GetProfileData);
 
 module.exports = router;
