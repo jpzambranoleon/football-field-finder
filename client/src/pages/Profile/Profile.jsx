a8619623ce5f6eaee04da5c3c4de5ddd9cf792bf
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-10 01:33:51 -0500

    deleted MyPosts.jsx and updated Profile.jsx

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,13 +1,59 @@
-import { Box, Container, Grid, Tab, Tabs } from "@mui/material";
-import React, { useContext } from "react";
+import { Box, Container, Grid, Pagination, Tab, Tabs } from "@mui/material";
+import axios from "axios";
+import { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Post from "../../components/Post";
 import { InfoContext } from "../../utils/InfoProvider";
 import Bio from "./components/Bio";
-import MyPosts from "./components/MyPosts";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box>{children}</Box>}
+    </div>
+  );
+}
 
 export default function Profile() {
-  const { authorizedUser } = useContext(InfoContext);
+  const [user, setUser] = useState({});
+  const [posts, setPosts] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [filterData, setFilterData] = useState({
+    page: 1,
+  });
+  const { username } = useParams();
+  const { setStatus } = useContext(InfoContext);
 
-  const [value, setValue] = React.useState("one");
+  useEffect(() => {
+    const fetchPosts = async () => {
+      axios
+        .get("/posts/my_posts/" + username, { params: filterData })
+        .then((res) => {
+          setPosts(res.data.posts);
+          setTotalPages(res.data.totalPages);
+        })
+        .catch((err) => {
+          let message = err.response ? err.response.data.message : err.message;
+          setStatus({ open: true, message: message, severity: "error" });
+        });
+    };
+    fetchPosts();
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?username=${username}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [setStatus, filterData, username]);
+
+  const [value, setValue] = useState("one");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -19,7 +65,7 @@ export default function Profile() {
         <Container>
           <Grid container spacing={3}>
             <Grid item xs={12} sm={3}>
-              <Bio authorizedUser={authorizedUser} />
+              <Bio user={user} />
             </Grid>
             <Grid item xs={12} sm={9}>
               <Box sx={{ borderBottom: 1, borderColor: "divider", mb: 2 }}>
@@ -33,7 +79,32 @@ export default function Profile() {
                   <Tab value="one" label="My Posts" />
                 </Tabs>
               </Box>
-              <MyPosts username={authorizedUser.username} />
+              <TabPanel>
+                <Grid container spacing={2}>
+                  {posts.map((p) => (
+                    <Post key={p._id} post={p} />
+                  ))}
+                </Grid>
+                <Box
+                  sx={{
+                    justifyContent: "center",
+                    display: "flex",
+                    mt: 2,
+                  }}
+                >
+                  <Pagination
+                    count={totalPages}
+                    onChange={(_, page) => {
+                      setFilterData((prevState) => ({
+                        ...prevState,
+                        page: page,
+                      }));
+                    }}
+                    variant="outlined"
+                    color="primary"
+                  />
+                </Box>
+              </TabPanel>
             </Grid>
           </Grid>
         </Container>
