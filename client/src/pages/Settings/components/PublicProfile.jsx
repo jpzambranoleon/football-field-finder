9d4fc38c5dea7af70b048f1ed9752aec5f360d82
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-10 23:30:19 -0500

    updated to add axios function to udate user info

--- client/src/pages/Settings/components/PublicProfile.jsx
+++ client/src/pages/Settings/components/PublicProfile.jsx
@@ -8,8 +8,48 @@ import {
   TextField,
 } from "@mui/material";
 import { AddPhotoAlternate } from "@mui/icons-material";
+import { InfoContext } from "../../../utils/InfoProvider";
+import { useContext } from "react";
+import axios from "axios";
+import { useState } from "react";
 
 const PublicProfile = ({ user }) => {
+  const { setStatus, authorizedUser } = useContext(InfoContext);
+  const [formData, setFormData] = useState({
+    name: `${user.name}`,
+    profilePicture: `${user.profilePicture}`,
+    email: `${user.email}`,
+    bio: `${user.bio}`,
+    location: `${user.location}`,
+  });
+
+  const [image, setImage] = useState("");
+
+  const handleChange = (e) => {
+    console.log(e.target.files);
+    setImage(e.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .put(`/users/update/${authorizedUser._id}`, {
+        userId: authorizedUser._id,
+        data: formData,
+      })
+      .then((res) => {
+        setStatus({
+          open: true,
+          message: res.data.message,
+          severity: "success",
+        });
+      })
+      .catch((err) => {
+        let message = err.response ? err.response.data.message : err.message;
+        setStatus({ open: true, message: message, severity: "error" });
+      });
+  };
+
   return (
     <>
       <Box
@@ -25,12 +65,31 @@ const PublicProfile = ({ user }) => {
       </Box>
       <Grid container spacing={8}>
         <Grid item xs={12} sm={7}>
-          <Box>
+          <Box component="form">
             <Typography variant="h7" fontWeight={600}>
               Name
             </Typography>
             <TextField
-              defaultValue={user.name}
+              defaultValue={authorizedUser.name}
+              id="name"
+              onChange={(e) =>
+                setFormData((prev) => ({ ...prev, name: e.target.value }))
+              }
+              fullWidth
+              multiline
+              rows={1}
+              size="small"
+              sx={{ mb: 2 }}
+            />
+            <Typography variant="h7" fontWeight={600}>
+              Public Email
+            </Typography>
+            <TextField
+              defaultValue={authorizedUser.email}
+              id="email"
+              onChange={(e) =>
+                setFormData((prev) => ({ ...prev, email: e.target.value }))
+              }
               fullWidth
               multiline
               rows={1}
@@ -41,7 +100,11 @@ const PublicProfile = ({ user }) => {
               Bio
             </Typography>
             <TextField
-              defaultValue={user.bio}
+              defaultValue={authorizedUser.bio}
+              id="bio"
+              onChange={(e) => {
+                setFormData((prev) => ({ ...prev, bio: e.target.value }));
+              }}
               fullWidth
               multiline
               rows={3}
@@ -51,7 +114,11 @@ const PublicProfile = ({ user }) => {
               Location
             </Typography>
             <TextField
-              defaultValue={user.location}
+              defaultValue={authorizedUser.location}
+              id="location"
+              onChange={(e) =>
+                setFormData((prev) => ({ ...prev, location: e.target.value }))
+              }
               fullWidth
               multiline
               rows={1}
@@ -61,6 +128,7 @@ const PublicProfile = ({ user }) => {
               variant="contained"
               size="small"
               color="success"
+              onClick={handleSubmit}
               sx={{ textTransform: "none", mt: 3 }}
             >
               Update Profile
@@ -72,13 +140,27 @@ const PublicProfile = ({ user }) => {
             Profile picture
           </Typography>
           <Box>
-            <Avatar sx={{ width: 200, height: 200 }} />
+            <Avatar
+              src={user.profilePicture}
+              sx={{ width: 200, height: 200 }}
+            />
             <Box sx={{ position: "absolute" }}>
               <IconButton
                 variant="contained"
                 color="primary"
                 sx={{ bottom: 60 }}
+                component="label"
               >
+                <input
+                  accept="image/*"
+                  type="file"
+                  onChange={(e) =>
+                    setFormData((prev) => ({
+                      ...prev,
+                      profilePicture: e.target.value,
+                    }))
+                  }
+                />
                 <AddPhotoAlternate fontSize="large" />
               </IconButton>
             </Box>
