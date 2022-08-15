aef5d6801dffc48f81b3178c0214cdb7b9b67aba
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-15 08:52:03 -0500

    updated FeedFilter.jsx

--- client/src/pages/Home/components/FeedFilter.jsx
+++ client/src/pages/Home/components/FeedFilter.jsx
@@ -8,85 +8,15 @@ import {
   FormGroup,
   IconButton,
   InputBase,
-  InputLabel,
-  MenuItem,
   Paper,
-  Select,
-  TextField,
 } from "@mui/material";
 import { useState } from "react";
-
-const ITEM_HEIGHT = 38;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 100,
-    },
-  },
-};
-
-const states = [
-  "AL",
-  "AK",
-  "AZ",
-  "AR",
-  "CA",
-  "CO",
-  "CT",
-  "DE",
-  "FL",
-  "GA",
-  "HI",
-  "ID",
-  "IL",
-  "IN",
-  "IA",
-  "KS",
-  "KY",
-  "LA",
-  "ME",
-  "MD",
-  "MA",
-  "MI",
-  "MN",
-  "MS",
-  "MO",
-  "MT",
-  "NE",
-  "NV",
-  "NH",
-  "NJ",
-  "NM",
-  "NY",
-  "NC",
-  "ND",
-  "OH",
-  "OK",
-  "OR",
-  "PA",
-  "RI",
-  "SC",
-  "SD",
-  "TN",
-  "TX",
-  "UT",
-  "VT",
-  "VA",
-  "WA",
-  "WV",
-  "WI",
-  "WY",
-];
 
 const FeedFilter = () => {
   const [filters, setFilters] = useState({
     team: true,
     trainer: true,
     player: true,
-    state: "",
-    city: "",
     name: "",
   });
 
@@ -147,35 +77,6 @@ const FeedFilter = () => {
           />
         </FormGroup>
       </FormControl>
-      <FormControl sx={{ ml: 1, minWidth: 100 }} size="small">
-        <InputLabel id="simple-select-label">State</InputLabel>
-        <Select
-          labelId="simple-select-label"
-          id="simple-select"
-          defaultValue={states[0]}
-          label="State"
-          MenuProps={MenuProps}
-          onChange={(e) => {
-            setFilters({ ...filters, state: e.target.value });
-          }}
-        >
-          {states.map((name) => (
-            <MenuItem key={name} value={name}>
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <TextField
-        id="outlined-basic"
-        label="City"
-        onChange={(e) => {
-          setFilters({ ...filters, city: e.target.value });
-        }}
-        variant="outlined"
-        size="small"
-        sx={{ ml: 1, width: 150 }}
-      />
       <Box sx={{ display: "flex", alignItems: "center", flex: 1 }}>
         <InputBase
           sx={{ ml: 1, flex: 1 }}
