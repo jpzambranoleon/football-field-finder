d401aef4b47d40053e0612f47977500bf66e0893
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-17 00:03:40 -0500

    updated index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 const MongooseConnection = require("./utility/mongoose.connection");
 const cors = require("cors");
 const morgan = require("morgan");
+const multer = require("multer");
 
 // Create application with express
 const app = express();
@@ -16,6 +17,21 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("common"));
+
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: fileStorageEngine });
+app.post("/api/single", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  res.send("Single file upload success");
+});
 
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/users.routes"));
