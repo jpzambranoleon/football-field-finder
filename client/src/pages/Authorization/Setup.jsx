821fc8a3db4de150c8096440a509acd7ed4fb7c0
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-18 00:54:58 -0500

    removed console.log(data)

--- client/src/pages/Authorization/Setup.jsx
+++ client/src/pages/Authorization/Setup.jsx
@@ -37,8 +37,6 @@ export default function Setup() {
       data.append("image", file);
       newUser.profilePic = fileName;
 
-      console.log(data);
-
       axios.post("/upload", data);
     }
     axios
