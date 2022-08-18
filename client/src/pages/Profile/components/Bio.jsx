cfaee28c034f9325b67ca8cd832a073f992a8c59
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-18 02:21:08 -0500

    updated Bio.jsx

--- client/src/pages/Profile/components/Bio.jsx
+++ client/src/pages/Profile/components/Bio.jsx
@@ -1,9 +1,12 @@
 import { LocationOnOutlined, MailOutlineOutlined } from "@mui/icons-material";
 import { Avatar, Box, Button, Typography } from "@mui/material";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { InfoContext } from "../../../utils/InfoProvider";
 
 const Bio = ({ user }) => {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { authorizedUser } = useContext(InfoContext);
 
   return (
     <Box>
@@ -24,39 +27,48 @@ const Bio = ({ user }) => {
       >
         {user.username}
       </Typography>
-      <Typography>{user.bio}</Typography>
-      <Button
-        component={Link}
-        to="/settings/profile"
-        fullWidth
-        color="primary"
-        variant="outlined"
-        sx={{ mt: 2 }}
-      >
-        Edit Profile
-      </Button>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          mt: 2,
-        }}
-      >
-        <LocationOnOutlined
+      <Typography sx={{ mb: 2 }}>{user.bio}</Typography>
+      {authorizedUser._id === user._id ? (
+        <Button
+          component={Link}
+          to="/settings/profile"
+          fullWidth
           color="primary"
-          fontSize="small"
-          sx={{ mr: 1, ml: -0.5 }}
-        />
-        <Typography variant="body2">{user.location}</Typography>
-      </Box>
-      <Box sx={{ display: "flex", alignItems: "center", mt: 1 }}>
-        <MailOutlineOutlined
-          color="primary"
-          fontSize="small"
-          sx={{ mr: 1, ml: -0.5 }}
-        />
-        <Typography variant="body2">{user.email}</Typography>
-      </Box>
+          variant="outlined"
+        >
+          Edit Profile
+        </Button>
+      ) : (
+        <Button fullWidth color="primary" variant="outlined">
+          Follow
+        </Button>
+      )}
+      {!user.location ? null : (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            mt: 2,
+          }}
+        >
+          <LocationOnOutlined
+            color="primary"
+            fontSize="small"
+            sx={{ mr: 1, ml: -0.5 }}
+          />
+          <Typography variant="body2">{user.location}</Typography>
+        </Box>
+      )}
+      {!user.email ? null : (
+        <Box sx={{ display: "flex", alignItems: "center", mt: 1 }}>
+          <MailOutlineOutlined
+            color="primary"
+            fontSize="small"
+            sx={{ mr: 1, ml: -0.5 }}
+          />
+          <Typography variant="body2">{user.email}</Typography>
+        </Box>
+      )}
     </Box>
   );
 };
