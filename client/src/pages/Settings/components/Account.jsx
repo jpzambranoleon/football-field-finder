18a8f41a34593c61865a87be47bed6b8ee93bd89
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-27 22:50:41 -0500

    added axios functions

--- client/src/pages/Settings/components/Account.jsx
+++ client/src/pages/Settings/components/Account.jsx
@@ -1,6 +1,88 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
+import { useContext } from "react";
+import { InfoContext } from "../../../utils/InfoProvider";
+import DeleteAccountButton from "./DeleteAccountButton";
 
 const Account = () => {
+  const { setStatus, authorizedUser } = useContext(InfoContext);
+  console.log(authorizedUser);
+  const [usernameData, setUsernameData] = useState({
+    username: "",
+  });
+
+  const [passwordData, setPasswordData] = useState({
+    oldPassword: "",
+    newPassword: "",
+    newPasswordConfirmed: "",
+  });
+
+  const handleChangeUsernameSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .put(`/users/update/username/${authorizedUser._id}`, {
+        userId: authorizedUser._id,
+        data: usernameData,
+      })
+      .then((res) => {
+        setStatus({
+          open: true,
+          message: res.data.message,
+          severity: "success",
+        });
+        window.location.reload();
+      })
+      .catch((err) => {
+        let message = err.response ? err.response.data.message : err.message;
+        setStatus({ open: true, message: message, severity: "error" });
+      });
+  };
+
+  const handleChangePasswordSubmit = (event) => {
+    event.preventDefault();
+    const oldPassword = passwordData.oldPassword;
+    const newPassword = passwordData.newPassword;
+    const newPasswordConfirmed = passwordData.newPasswordConfirmed;
+    axios
+      .put(`/users/update/password/${authorizedUser._id}`, {
+        userId: authorizedUser._id,
+        oldPassword: oldPassword,
+        newPassword: newPassword,
+        data: newPasswordConfirmed,
+      })
+      .then((res) => {
+        setStatus({
+          open: true,
+          message: res.data.message,
+          severity: "success",
+        });
+        window.location.reload();
+      })
+      .catch((err) => {
+        let message = err.response ? err.response.data.message : err.message;
+        setStatus({ open: true, message: message, severity: "error" });
+      });
+  };
+
+  const deleteAccount = (event) => {
+    event.preventDefault();
+    axios
+      .delete(`/users/delete/${authorizedUser._id}`, {
+        userId: authorizedUser._id,
+      })
+      .then((res) => {
+        setStatus({
+          open: true,
+          message: res.data.message,
+          severity: "success",
+        }).catch((err) => {
+          let message = err.response ? err.response.data.message : err.message;
+          setStatus({ open: true, message: message, severity: "error" });
+        });
+      });
+  };
+
   return (
     <>
       <Box
@@ -14,11 +96,44 @@ const Account = () => {
           Change username
         </Typography>
       </Box>
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <Typography variant="h7" fontWeight={600}>
+            Current username
+          </Typography>
+          <TextField
+            defaultValue={authorizedUser.username}
+            disabled
+            id="username"
+            fullWidth
+            multiline
+            rows={1}
+            size="small"
+            sx={{ mb: 2 }}
+          />
+          <Typography variant="h7" fontWeight={600}>
+            New username
+          </Typography>
+          <TextField
+            id="new_username"
+            name="new_username"
+            fullWidth
+            multiline
+            rows={1}
+            size="small"
+            onChange={(e) =>
+              setUsernameData((prev) => ({ ...prev, username: e.target.value }))
+            }
+            sx={{ mb: 2 }}
+          />
+        </Grid>
+      </Grid>
       <Button
         variant="contained"
         size="small"
         color="success"
-        sx={{ textTransform: "none" }}
+        onClick={handleChangeUsernameSubmit}
+        sx={{ textTransform: "none", mb: 5 }}
       >
         Change username
       </Button>
@@ -27,27 +142,79 @@ const Account = () => {
           Change password
         </Typography>
       </Box>
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <Typography variant="h7" fontWeight={600}>
+            Old Password
+          </Typography>
+          <TextField
+            fullWidth
+            id="password"
+            type="password"
+            name="password"
+            size="small"
+            onChange={(e) =>
+              setPasswordData((prev) => ({
+                ...prev,
+                oldPassword: e.target.value,
+              }))
+            }
+            sx={{ mb: 2 }}
+          />
+          <Typography variant="h7" fontWeight={600}>
+            New password
+          </Typography>
+          <TextField
+            fullWidth
+            id="new_password"
+            type="password"
+            name="new_password"
+            size="small"
+            onChange={(e) =>
+              setPasswordData((prev) => ({
+                ...prev,
+                newPassword: e.target.value,
+              }))
+            }
+            sx={{ mb: 2 }}
+          />
+          <Typography variant="h7" fontWeight={600}>
+            Confirm new password
+          </Typography>
+          <TextField
+            fullWidth
+            id="confirm_new_password"
+            type="password"
+            name="confirm_new_password"
+            size="small"
+            onChange={(e) =>
+              setPasswordData((prev) => ({
+                ...prev,
+                newPasswordConfirmed: e.target.value,
+              }))
+            }
+            sx={{ mb: 2 }}
+          />
+        </Grid>
+      </Grid>
       <Button
         variant="contained"
         size="small"
         color="success"
-        sx={{ textTransform: "none" }}
+        onClick={handleChangePasswordSubmit}
+        sx={{ textTransform: "none", mb: 5 }}
       >
         Change password
       </Button>
       <Box sx={{ borderBottom: 1, borderColor: "divider", mb: 2 }}>
         <Typography variant="h5" gutterBottom color="error">
-          Delete User
+          Delete account
         </Typography>
       </Box>
-      <Button
-        color="error"
-        variant="contained"
-        size="small"
-        sx={{ textTransform: "none" }}
-      >
-        Delete user
-      </Button>
+      <Typography variant="body1" color="text.secondary" gutterBottom>
+        Once you delete your account, there is no goin back. Please be certain.
+      </Typography>
+      <DeleteAccountButton />
     </>
   );
 };
