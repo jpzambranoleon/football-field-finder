872713befba9c200b700ecbced9b0d28b40d406c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-08-27 22:51:38 -0500

    added two new user routes

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -5,8 +5,12 @@ const { validateToken } = require("../middlewares/validate.jwt.middleware");
 router.put("/set", UserController.setPublicInfo);
 // UPDATE USER
 router.put("/update/:id", UserController.updateUser);
+// UPDATE USERNAME
+router.put("/update/username/:id", UserController.updateUsername);
+// CHANGE PASSWORD
+router.put("/update/password/:id", UserController.changePassword);
 // DELETE USER
-router.delete("/:id", UserController.deleteUser);
+router.delete("/delete/:id", UserController.deleteUser);
 // GET USER
 router.get("/", UserController.getUser);
 
