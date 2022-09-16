d9215ef2335f43df9dc972059bd00fe6cab9f96a
Jean-Paul <jpzambrano.leon@gmail.com>
2022-09-15 22:24:09 -0500

    imported GoogleRecaptchaProvider

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Setup from "./pages/Auth/Setup";
 import Activate from "./pages/Auth/Activate";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 function App() {
   axios.defaults.baseURL = "http://localhost:8800/api";
@@ -27,42 +28,44 @@ function App() {
   const { authorized } = useContext(InfoContext);
 
   return (
-    <div className="App">
-      <>
-        <Router>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              minHeight: "100vh",
-            }}
-          >
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/post/view/:postId" element={<ViewPost />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/setup" element={<Setup />} />
-              <Route path="/register/activate" element={<Activate />} />
-              <Route path="/:username" element={<Profile />} />
-              {authorized ? (
-                <>
-                  <Route path="/post/create" element={<CreatePost />} />
-                  <Route
-                    path="/settings"
-                    element={<Navigate to="/settings/profile" />}
-                  />
-                  <Route path="/settings/:page" element={<Settings />} />
-                </>
-              ) : null}
-            </Routes>
-            <StickyFooter />
-          </Box>
-        </Router>
-      </>
-    </div>
+    <GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_CAPTCHA_KEY}>
+      <div className="App">
+        <>
+          <Router>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                minHeight: "100vh",
+              }}
+            >
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/post/view/:postId" element={<ViewPost />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/setup" element={<Setup />} />
+                <Route path="/register/activate" element={<Activate />} />
+                <Route path="/:username" element={<Profile />} />
+                {authorized ? (
+                  <>
+                    <Route path="/post/create" element={<CreatePost />} />
+                    <Route
+                      path="/settings"
+                      element={<Navigate to="/settings/profile" />}
+                    />
+                    <Route path="/settings/:page" element={<Settings />} />
+                  </>
+                ) : null}
+              </Routes>
+              <StickyFooter />
+            </Box>
+          </Router>
+        </>
+      </div>
+    </GoogleReCaptchaProvider>
   );
 }
 
