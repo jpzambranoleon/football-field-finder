99402aacc760793f3b524b3a514cb36e76174da6
Jean-Paul <jpzambrano.leon@gmail.com>
2022-09-17 18:27:56 -0500

    added link to forgot password

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -97,7 +97,7 @@ export default function Login() {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
+                  <Link href="/password/forgot" variant="body2">
                     Forgot password?
                   </Link>
                 </Grid>
