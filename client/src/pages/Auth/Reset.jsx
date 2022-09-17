a7ad1d72c25d2955f96c9503f814157e8212e219
Jean-Paul <jpzambrano.leon@gmail.com>
2022-09-17 18:28:41 -0500

    added handleFormChange function

--- client/src/pages/Auth/Reset.jsx
+++ client/src/pages/Auth/Reset.jsx
@@ -1,8 +1,16 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  TextField,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useContext } from "react";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
+import { useParams } from "react-router-dom";
 import { InfoContext } from "../../utils/InfoProvider";
 
 export default function Reset(props) {
@@ -11,6 +19,12 @@ export default function Reset(props) {
   const { setStatus } = useContext(InfoContext);
   const { executeRecaptcha } = useGoogleReCaptcha();
   const { token } = useParams();
+
+  const handleFormChange = () => {
+    setFormData({
+      ...Object.fromEntries(new FormData(document.getElementById("auth"))),
+    });
+  };
 
   const handleReCaptchaVerify = useCallback(async () => {
     if (!executeRecaptcha) {
@@ -43,5 +57,54 @@ export default function Reset(props) {
       });
   };
 
-  return <Box></Box>;
+  return (
+    <main>
+      <Box sx={{ bgcolor: "background.paper", pt: 4, pb: 6 }}>
+        <Container maxWidth="xs">
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Password Reset
+            </Typography>
+            <Box
+              component="form"
+              id="auth"
+              onSubmit={handleFormSubmit}
+              onChange={handleFormChange}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                name="newPassword"
+                type="password"
+              />
+              {loading ? (
+                <CircularProgress />
+              ) : (
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, textTransform: "none" }}
+                >
+                  Submit
+                </Button>
+              )}
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+    </main>
+  );
 }
