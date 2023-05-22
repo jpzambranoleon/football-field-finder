e4d5f669c8c27fc3243fe99f66b1bd1bcd3a741b
Jean-Paul <jpzambrano.leon@gmail.com>
2023-05-22 17:55:33 -0500

    created user.model.js

---
+++ models/user.model.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+
+      max: 20,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 10,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+    profilePicture: {
+      type: String,
+      default: "/broken-image.jpg",
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followings: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
