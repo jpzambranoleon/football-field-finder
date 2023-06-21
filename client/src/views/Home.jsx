d7b74adebb506577f78eeae0d0d05628b39b1a00
Jean-Paul <jpzambrano.leon@gmail.com>
2023-06-20 21:40:11 -0500

    modified file

---
+++ client/src/views/Home.jsx
@@ -0,0 +1,13 @@
+import BookCar from "../components/BookCar";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <BookCar />
+      <Footer />
+    </>
+  );
+}
